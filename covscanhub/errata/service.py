d27a79827a9f63bb513d0754508a98245e8a6a59
Tomas Tomecek <ttomecek@redhat.com>
2012-10-10 15:12:16 +0200

    refactorization of create_errata_scan

--- covscanhub/errata/service.py
+++ covscanhub/errata/service.py
@@ -10,7 +10,66 @@ from covscanhub.scan.service import run_diff
 from covscanhub.scan.models import Scan, SCAN_STATES, Tag
 from covscanhub.waiving.service import create_results
 from kobo.hub.models import Task
+import copy
+            
+def create_errata_base_scan(kwargs, task_id):
+    options = {}
 
+    task_user = kwargs['task_user']
+    username = kwargs['username']
+    scan_type = SCAN_STATES['ERRATA_BASE']
+    base_obj = None
+    nvr = kwargs['base']
+    task_label = nvr
+    
+    #TODO change this
+    tag = kwargs['tag']
+    tag_obj = Tag.objects.get(name=tag)
+    options['mock_config'] = tag_obj.mock.name
+    
+    priority = kwargs.get('priority', settings.ET_SCAN_PRIORITY) + 1
+    comment = 'Errata Tool Base scan of %s requested by %s' % \
+        (nvr, kwargs['base'])
+
+    # Test if SRPM exists
+    brew_proxy = brew.ClientSession(settings.BREW_HUB)
+    try:
+        brew_proxy.getBuild(nvr)
+        options['brew_build'] = nvr
+    except brew.GenericError:
+        raise RuntimeError("Brew build of package %s does not exist" % nvr)
+
+    task_id = Task.create_task(
+        owner_name=task_user,
+        label=task_label,
+        method='ErrataDiffBuild',
+        args={}, # I want to add scan's id here, so I update it later
+        comment=comment,
+        state=SCAN_STATES["QUEUED"],
+        priority=priority,
+        parent_id=task_id,
+    )
+    task_dir = Task.get_task_dir(task_id)
+
+    if not os.path.isdir(task_dir):
+        try:
+            os.makedirs(task_dir, mode=0755)
+        except OSError, ex:
+            if ex.errno != 17:
+                raise
+
+    # if base is specified, try to fetch it; if it doesn't exist, create
+    # new task for it
+    scan = Scan.create_scan(scan_type=scan_type, nvr=nvr, task_id=task_id,
+                            tag=tag_obj, base=base_obj, username=username)
+    scan.save()
+
+    options['scan_id'] = scan.id
+    task = Task.objects.get(id=task_id)
+    task.args = options
+    task.save()
+    
+    return scan
 
 def create_errata_scan(kwargs):
     """
@@ -46,11 +105,7 @@ def create_errata_scan(kwargs):
     priority = kwargs.get('priority', settings.ET_SCAN_PRIORITY)
 
     #if kwargs does not have 'id', it is base scan
-    if kwargs['id'] is None:
-        comment = 'Errata Tool Base scan generated for %s'\
-            % kwargs['requestor']
-    else:
-        comment = 'Errata Tool Scan of %s' % nvr
+    comment = 'Errata Tool Scan of %s' % nvr
 
     #does tag exist?
     try:
@@ -82,7 +137,6 @@ def create_errata_scan(kwargs):
         comment=comment,
         state=SCAN_STATES["QUEUED"],
         priority=priority,
-        parent_id=kwargs.get('parent_task', None),
     )
     task_dir = Task.get_task_dir(task_id)
 
@@ -96,24 +150,12 @@ def create_errata_scan(kwargs):
     # if base is specified, try to fetch it; if it doesn't exist, create
     # new task for it
     base_obj = None
-    base_scan = None
     if base:
         try:
             base_obj = Scan.objects.get(nvr=base)
         except ObjectDoesNotExist:
-            import copy
-            o = copy.deepcopy(kwargs)
-            o['nvr'] = base
-            o['base'] = None
-            o['requestor'] = nvr
-            o['priority'] = o.get('priority', settings.ET_SCAN_PRIORITY) + 1
-            o['id'] = None
-            o['parent_task'] = task_id
-            #TODO base tag vs. parent tag
-
-            parent_task = Task.objects.get(id=task_id)
-
-            base_scan = create_errata_scan(o)
+            parent_task = Task.objects.get(id=task_id)            
+            base_obj = create_errata_base_scan(copy.deepcopy(kwargs), task_id)
 
             # wait has to be after creation of new subtask
             # TODO wait should be executed in one transaction with creation of
@@ -125,10 +167,6 @@ def create_errata_scan(kwargs):
 
     scan = Scan.create_scan(scan_type=scan_type, nvr=nvr, task_id=task_id,
                             tag=tag_obj, base=base_obj, username=username)
-
-    if base_scan is not None:    
-        scan.base = base_scan
-        scan.save()
 
     options['scan_id'] = scan.id
     task = Task.objects.get(id=task_id)
