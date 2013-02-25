7688ef8a4faee3f92fe79db6b487387545cf474c
Tomas Tomecek <ttomecek@redhat.com>
2013-02-25 10:42:32 +0100

    Fix redirect for required authentication.

--- covscanhub/middleware.py
+++ covscanhub/middleware.py
@@ -4,6 +4,9 @@ from django.http import HttpResponseRedirect
 from django.conf import settings
 from re import compile
 
+from django.core.urlresolvers import reverse
+
+
 EXEMPT_URLS = [compile(settings.LOGIN_URL.lstrip('/'))]
 if hasattr(settings, 'LOGIN_EXEMPT_URLS'):
     EXEMPT_URLS += [compile(expr) for expr in settings.LOGIN_EXEMPT_URLS]
@@ -29,4 +32,5 @@ class LoginRequiredMiddleware(object):
         if not request.user.is_authenticated():
             path = request.path_info.lstrip('/')
             if not any(m.match(path) for m in EXEMPT_URLS):
-                return HttpResponseRedirect(settings.LOGIN_URL)
+                url = reverse(settings.LOGIN_URL)
+                return HttpResponseRedirect(url + "?next=" + request.path)
