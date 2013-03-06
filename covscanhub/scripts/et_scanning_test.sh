0f62ec21e255e06cfd105b78a0d01e5971f000b9
Tomas Tomecek <ttomecek@redhat.com>
2013-03-06 16:30:20 +0100

    Script with some ET scans.

---
+++ covscanhub/scripts/et_scanning_test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./xmlrpc.py -b wget-1.11.4-4.el6 -t wget-1.12-1.8.el6
+./xmlrpc.py -b coreutils-8.4-5.el6 -t coreutils-8.4-19.el6
+./xmlrpc.py -b hardlink-1.0-9.el6 -t hardlink-1.0-10.el6
+./xmlrpc.py -b NEW_PACKAGE -t tar-1.23-11.el6
