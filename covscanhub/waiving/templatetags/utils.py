167ee299f72b3511384e44fb7f15e5232be70f58
Tomas Tomecek <ttomecek@redhat.com>
2013-03-14 09:39:17 +0100

    Make dates and times human readable.

--- covscanhub/waiving/templatetags/utils.py
+++ covscanhub/waiving/templatetags/utils.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from datetime import date, datetime
+
 from django import template
+from django.template import defaultfilters
 from django.utils.safestring import mark_safe
+from django.utils.translation import pgettext, ungettext, ugettext as _
+#from django.utils.timezone import is_aware, utc
 
 register = template.Library()
 
@@ -47,3 +52,65 @@ def line_and_column(obj):
     if "column" in obj:
         response += ':' + str(obj['column'])
     return response
+
+
+@register.filter
+def naturaltime(value):
+    """
+    taken from django:master
+
+    For date and time values shows how many seconds, minutes or hours ago
+    compared to current timestamp returns representing string.
+
+    This filter doesn't require expects_localtime=True because it deals properly
+    with both naive and aware datetimes. Therefore avoid the cost of conversion.
+    """
+    if not isinstance(value, date):  # datetime is a subclass of date
+        return value
+
+    now = datetime.now()  # (utc if is_aware(value) else None)
+    if value < now:
+        delta = now - value
+        if delta.days != 0:
+            return pgettext(
+                'naturaltime', '%(delta)s ago'
+            ) % {'delta': defaultfilters.timesince(value, now)}
+        elif delta.seconds == 0:
+            return _('now')
+        elif delta.seconds < 60:
+            return ungettext(
+                'a second ago', '%(count)s seconds ago', delta.seconds
+            ) % {'count': delta.seconds}
+        elif delta.seconds // 60 < 60:
+            count = delta.seconds // 60
+            return ungettext(
+                'a minute ago', '%(count)s minutes ago', count
+            ) % {'count': count}
+        else:
+            count = delta.seconds // 60 // 60
+            return ungettext(
+                'an hour ago', '%(count)s hours ago', count
+            ) % {'count': count}
+    else:
+        delta = value - now
+        if delta.days != 0:
+            return pgettext(
+                'naturaltime', '%(delta)s from now'
+            ) % {'delta': defaultfilters.timeuntil(value, now)}
+        elif delta.seconds == 0:
+            return _('now')
+        elif delta.seconds < 60:
+            return ungettext(
+                'a second from now', '%(count)s seconds from now',
+                delta.seconds
+            ) % {'count': delta.seconds}
+        elif delta.seconds // 60 < 60:
+            count = delta.seconds // 60
+            return ungettext(
+                'a minute from now', '%(count)s minutes from now', count
+            ) % {'count': count}
+        else:
+            count = delta.seconds // 60 // 60
+            return ungettext(
+                'an hour from now', '%(count)s hours from now', count
+            ) % {'count': count}
