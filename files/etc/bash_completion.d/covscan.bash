ac26e563310741ea8953f56d2a8022ef8ef745c4
Tomas Tomecek <ttomecek@redhat.com>
2013-03-15 15:11:02 +0100

    Set file completion to default.

--- files/etc/bash_completion.d/covscan.bash
+++ files/etc/bash_completion.d/covscan.bash
@@ -5,6 +5,29 @@ _covscan_configs()
         main()" 2>/dev/null)
 }
 
+#function contains() {
+#    local n=$#
+#    local value=${!n}
+#    for ((i=1;i < $#;i++)) {
+#        if [ "${!i}" == "${value}" ]; then
+#            echo "y"
+#            return 0
+#        fi
+#    }
+#    echo "n"
+#    return 1
+#}
+#
+#function is_build_scan() {
+#    local build_opts=("mock-build" "diff-build" "version-diff-build")
+#    for $opt in $build_opts ; do
+#        if [ $(contains "$#" "${opt}") == "y" ]; then
+#            return 0
+#        fi
+#    done
+#    return 1
+#}
+
 _covscan() 
 {
     local cur prev prev2 opts
@@ -26,9 +49,13 @@ _covscan()
         COMPREPLY=( $(compgen -W "--config=" -- ${cur}) )
         compopt -o nospace
         return 0
+    elif [[ ${#COMP_WORDS[@]} < 3 ]] ; then
+        COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
+        return 0
+    #elif [ $( is_build_scan "${COMP_WORDS[@]}" ) ] ; then
     fi
-
-   COMPREPLY=($(compgen -W "${opts}" -- ${cur}))  
-   return 0
+    COMPREPLY=( $( compgen -f -- "$cur") )
+    compopt -o filenames
+    return 0
 }
 complete -F _covscan covscan
