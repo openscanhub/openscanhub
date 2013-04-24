56c4e12f2c59c492edcc60409cf9d50a4d044c24
Tomas Tomecek <ttomecek@redhat.com>
2013-04-24 15:13:46 +0200

    Fill server info only when creds are specified.

--- covscan/utils/conf.py
+++ covscan/utils/conf.py
@@ -103,14 +103,16 @@ class Conf(object):
         """
         Return name of default MockConfig
         """
-        cim_data =  {
+        cim_data = {
             'user': self.conf.get('CIM', 'user', False, {'user': None}),
             'passwd': self.conf.get('CIM', 'password', False, {'password': None}),
             'server': self.conf.get('CIM', 'server', False, {'server': None}),
             'port': self.conf.get('CIM', 'port', False, {'port': None}),
             'stream': self.conf.get('CIM', 'stream', False, {'stream': None}),
         }
-        if cim_data['server'] is None:
+        # when user provides only credentials and not server:port, use default
+        # from system config
+        if cim_data['user'] is not None and cim_data['server'] is None:
             cim_data['server'] = self.system_conf['CIM_SERVER']
             cim_data['port'] = self.system_conf['CIM_PORT']
 
