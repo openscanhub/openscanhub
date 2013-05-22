83d2dfdcee746412cc2dd9eedf4ad7088248bff1
Tomas Tomecek <ttomecek@redhat.com>
2013-05-22 17:26:42 +0200

    Add new cmd, find-tasks + new supporting xmlrpc call.

---
+++ covscan/commands/cmd_find_task.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import covscan
+
+
+class Find_Tasks(covscan.CovScanCommand):
+    """find tasks by provided query string"""
+    enabled = True
+    admin = False  # admin type account required
+
+    def options(self):
+        # specify command usage
+        # normalized name contains a lower-case class name with underscores
+        # converted to dashes
+        self.parser.usage = "%%prog %s [options] <query_string>" % \
+            self.normalized_name
+        self.parser.epilog = "without '-l' option, newest task is at the \
+beginning of a list, unfinished tasks are at the end; you should pick one of \
+these options: --regex, --package, --nvr"
+        self.parser.add_option(
+            "-l",
+            "--latest",
+            default=False,
+            action="store_true",
+            help="display only latest task",
+        )
+        self.parser.add_option(
+            "-r",
+            "--regex",
+            default=False,
+            action="store_true",
+            help="query by specified regex",
+        )
+        self.parser.add_option(
+            "-p",
+            "--package",
+            default=False,
+            action="store_true",
+            help="return only scans of provided package",
+        )
+        self.parser.add_option(
+            "-n",
+            "--nvr",
+            default=False,
+            action="store_true",
+            help="show scans of specific build, specified by NVR (default one)"
+        )
+
+    def run(self, *args, **kwargs):
+        #local_conf = get_conf(self.conf)
+
+        username = kwargs.pop("username", None)
+        password = kwargs.pop("password", None)
+
+        regex = kwargs.pop("regex")
+        package_name = kwargs.pop("package")
+
+        latest = kwargs.pop("latest")
+
+        if len(args) != 1:
+            self.parser.error("please specify exactly one query string \
+(in case of regex, enclose it in quotes please])")
+
+        query_string = args[0]
+
+        # login to the hub
+        self.set_hub(username, password)
+
+        query = {}
+        if regex:
+            query['regex'] = query_string
+        elif package_name:
+            query['package_name'] = query_string
+        else:
+            # nvr is default one, so we don't care if it's specified
+            query['nvr'] = query_string
+        task_ids = self.hub.scan.find_tasks(query)
+
+        if task_ids:
+            if latest:
+                print task_ids[0]
+            else:
+                for task_id in task_ids:
+                    print task_id
+        else:
+            print 'There were no tasks found.'
+            sys.exit(1)
+        sys.exit(0)
--- covscanhub/xmlrpc/scan.py
+++ covscanhub/xmlrpc/scan.py
@@ -23,6 +23,7 @@ __all__ = (
     "create_user_diff_task",
     "create_base_scans",
     "get_task_info",
+    "find_tasks",
 )
 
 
@@ -213,3 +214,37 @@ def get_task_info(request, task_id):
         pass
     return result
 
+
+def find_tasks(request, query):
+    """
+    find_tasks(request, query) -> [ <id>, <id>, ... ]
+
+    Query hub to get IDs of tasks specified by query.
+    Query is a dict (hash/map), it has to have exactly one of these keys:
+     * 'package_name': return all task IDs for provided package
+     * 'nvr': search by specific NVR
+     * 'regex': find by provided regex (this is not match, but find, if you
+                want match, change your regex to "^<regex>$")
+
+    Returned list is ordered by date, when task finished -- latest task is
+    first. Unfinished tasks are at the tail. If there is any problem with
+    querying, empty list is returned.
+    """
+    if not isinstance(query, dict):
+        return []
+    nvr = query.get('nvr', None)
+    package_name = query.get('package_name', None)
+    regex = query.get('regex', None)
+
+    result = []
+    tasks = None
+    if nvr:
+        tasks = Task.objects.filter(label=nvr)
+    if package_name:
+        tasks = Task.objects.filter(label__regex=package_name + "-\d")
+    elif regex:
+        tasks = Task.objects.filter(label__regex=regex)
+    if tasks is not None:
+        result = list(tasks.order_by("-dt_finished").values_list(
+            "id", flat=True))
+    return result
