567df89f836299b1ec7402298f94cf4f32fa3821
Tomas Tomecek <ttomecek@redhat.com>
2013-05-22 17:20:38 +0200

    Add docs to mockconfig xmlrpc calls.

--- covscanhub/xmlrpc/mock_config.py
+++ covscanhub/xmlrpc/mock_config.py
@@ -12,10 +12,19 @@ __all__ = (
 
 
 def all(request):
+    """
+    Return list of mockconfigs:
+        [("mock_config_name", "is_enabled?"), ...]
+    """
     return list(MockConfig.objects.all().values("name", "enabled"))
 
 
 def get(request, name):
+    """
+    get(name) -> { "name": <name>, "enable": <True|False> }
+
+    Return info about mockconfig specified by its name.
+    """
     try:
         return MockConfig.objects.get(name=name).export()
     except ObjectDoesNotExist:
