31968b6e27a80a1c303bad765750331a81ce60ed
Tomas Tomecek <ttomecek@redhat.com>
2013-07-01 17:29:11 +0200

    remove duplicate field in ResultGroup admin

--- covscanhub/waiving/admin.py
+++ covscanhub/waiving/admin.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from models import DEFECT_STATES
+#from models import DEFECT_STATES
 
 from covscanhub.other.admin import register_admin_module
 
-defect_states = lambda self, instance: \
-    DEFECT_STATES.get_value(instance.defect_type)
-defect_states.short_description = 'Defects state'
+#defect_states = lambda self, instance: \
+#    DEFECT_STATES.get_value(instance.defect_type)
+#defect_states.short_description = 'Defects state'
 
-register_admin_module('covscanhub.waiving.models', new_fields={
-    'ResultGroup': ('defect_type_text', defect_states),
-})
+register_admin_module('covscanhub.waiving.models')  # new_fields={
+#    'ResultGroup': ('defect_type_text', defect_states),
+#})
