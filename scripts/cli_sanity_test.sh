646793df77fed62fd34a1b355145cee316b671fb
Tomas Tomecek <ttomecek@redhat.com>
2013-09-03 15:08:51 +0200

    add some sanity tests for client

--- scripts/cli_sanity_test.sh
+++ scripts/cli_sanity_test.sh
@@ -10,3 +10,16 @@ PKG=`ls *src.rpm`
 ./covscan/covscan version-diff-build --base-config=epel-6-x86_64 --config=epel-6-x86_64 --base-brew-build=units-1.87-2.fc9 --srpm=./$PKG --concurrency --nowait
 
 ./covscan/covscan version-diff-build --base-config=epel-6-x86_64 --config=epel-6-x86_64 --base-brew-build=units-1.87-2.fc9 --brew-build=units-1.87-7.el6 --all --nowait
+
+# --analyzer option
+
+./covscan/covscan mock-build --config=epel-6-x86_64 -a cov-6.6.1,clang,cppcheck -w 3 --brew-build hardlink-1.0-10.el6
+./covscan/covscan mock-build --config=epel-6-x86_64 -l --brew-build hardlink-1.0-10.el6
+./covscan/covscan mock-build --config=epel-6-x86_64 -l -c -w 2 --brew-build hardlink-1.0-10.el6
+./covscan/covscan mock-build --config=epel-6-x86_64 -b -w 3 --brew-build hardlink-1.0-10.el6
+./covscan/covscan mock-build --config=epel-6-x86_64 -a cov-x --brew-build hardlink-1.0-10.el6
+./covscan/covscan mock-build --config=epel-6-x86_64 -a cov-6.6.1,cov6.5.3 --brew-build hardlink-1.0-10.el6
+./covscan/covscan mock-build --config=epel-6-x86_64 -a cov-6.6.1,cov-6.5.3 --brew-build hardlink-1.0-10.el6
+./covscan/covscan mock-build --config=epel-6-x86_64 -a cov-6.6.1,clang,cppcheck -w 3 --brew-build dracut-032-1.fc20
+./covscan/covscan mock-build --config=fedora-20-x86_64 -a cov-6.6.1,clang,cppcheck -b -c -l --brew-build dracut-032-1.fc20
+
