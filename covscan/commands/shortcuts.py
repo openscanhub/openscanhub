openscanhub/openscanhub

commit 81b122cf3de7efcaf276bf766b16d10ed4d5478d

Hard coded brew/koji links changed to constants

covscan/commands/shortcuts.py
@@ -5,6 +5,7 @@ import os
 import re
 
 from xmlrpclib import Fault
+from django.conf import settings
 
 
 __all__ = (
@@ -94,7 +95,7 @@ def upload_file(hub, srpm, target_dir, parser):
 
 
 if __name__ == '__main__':
-    print verify_brew_koji_build('scipy-0.12.1-1.el7', 'http://brewhub.engineering.redhat.com/brewhub', 'http://koji.fedoraproject.org/kojihub')
-    print verify_brew_koji_build('xscipy-0.12.1-1.el7', 'http://brewhub.engineering.redhat.com/brewhub', 'http://koji.fedoraproject.org/kojihub')
-    print verify_brew_koji_build('gnome-shell-3.10.2.1-3.fc20', 'http://brewhub.engineering.redhat.com/brewhub', 'http://koji.fedoraproject.org/kojihub')
-    print verify_brew_koji_build('gnome-shell-3.10.1-2.fc21', 'http://brewhub.engineering.redhat.com/brewhub', 'http://koji.fedoraproject.org/kojihub')
+    print verify_brew_koji_build('scipy-0.12.1-1.el7', settings.BREW_URL, settings.KOJI_URL)
+    print verify_brew_koji_build('xscipy-0.12.1-1.el7', settings.BREW_URL, settings.KOJI_URL)
+    print verify_brew_koji_build('gnome-shell-3.10.2.1-3.fc20', settings.BREW_URL, settings.KOJI_URL)
+    print verify_brew_koji_build('gnome-shell-3.10.1-2.fc21', settings.BREW_URL, settings.KOJI_URL)

covscanhub/errata/caps.py
@@ -22,11 +22,7 @@ if __name__ == '__main__':
 else:
     logger = logging.getLogger(__name__)
 
-try:
-    s = koji.ClientSession(settings.MAIN_KOJI_BUILDSYSTEM[0])
-except (ImportError, NameError, ImproperlyConfigured):
-    s = koji.ClientSession("http://brewhub.engineering.redhat.com/brewhub")
-
+s = koji.ClientSession(settings.BREW_URL)
 
 class CapabilityChecker(object):
     def check(self, **kwargs):

covscanhub/errata/check.py
@@ -67,9 +67,10 @@ def check_obsolete_scan(package, release):
 
 
 def check_build(nvr, check_additional=False):
-    url, bin = settings.MAIN_KOJI_BUILDSYSTEM
+    url = settings.BREW_URL
+    bin = settings.BREW_BIN_NAME
     if check_additional:
-        add_bss = iter(settings.OTHER_KOJI_BUILDSYSTEMS)
+        add_bss = iter((settings.KOJI_URL, settings.KOJI_BIN_NAME))
     else:
         add_bss = iter([])
     not_found = True

covscanhub/errata/tests.py
@@ -7,6 +7,7 @@ import koji
 
 from covscanhub.errata.utils import get_mocks_repo
 from covscanhub.errata.caps import UnifiedCapabilityChecker, FileCapabilityChecker, RPMDepCapabilityChecker
+from django.conf import settings
 
 
 # TODO: implement own runner and set logging level to match --verbosity
@@ -77,7 +78,7 @@ class TestCap(unittest.TestCase):
         self.assertTrue(isinstance(get_mocks_repo('rhel-6-x86_64'), (basestring, list)))
 
     def test_c_brew(self):
-        s = koji.ClientSession("http://brewhub.engineering.redhat.com/brewhub")
+        s = koji.ClientSession(settings.BREW_URL)
         nvr = "rpm-4.11.1-8.el7"
         build = s.getBuild(nvr)
         rpms = s.listRPMs(buildID=build['id'])

covscanhub/errata/utils.py
@@ -28,10 +28,7 @@ if __name__ == '__main__':
 else:
     logger = logging.getLogger(__name__)
 
-try:
-    s = koji.ClientSession(settings.MAIN_KOJI_BUILDSYSTEM[0])
-except AttributeError:
-    s = koji.ClientSession("http://brewhub.engineering.redhat.com/brewhub")
+s = koji.ClientSession(settings.BREW_URL)
 
 
 def get_or_fail(key, data):

covscanhub/scripts/brew_interaction.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from pprint import pprint
+from django.conf import settings
 import brew
 import sys
 
@@ -10,7 +11,7 @@ try:
 except IndexError:
     nvr = 'bind-9.8.2-0.17.rc1.el6.3'
 
-session = brew.ClientSession('http://brewhub.engineering.redhat.com/brewhub')
+session = brew.ClientSession(settings.BREW_URL)
 build = session.getBuild(nvr)
 print 'Build:'
 pprint(build)

covscanhub/settings.py
@@ -178,8 +178,10 @@ XMLRPC_METHODS = {
 
 }
 
-MAIN_KOJI_BUILDSYSTEM = ('http://brewhub.engineering.redhat.com/brewhub', 'brew')
-OTHER_KOJI_BUILDSYSTEMS = [('http://koji.fedoraproject.org/kojihub', 'koji')]
+BREW_URL = 'http://brewhub.engineering.redhat.com/brewhub'
+BREW_BIN_NAME = 'brew'
+KOJI_URL = 'http://koji.fedoraproject.org/kojihub'
+KOJI_BIN_NAME = 'koji'
 ET_SCAN_PRIORITY = 20
 
 VALID_TASK_LOG_EXTENSIONS = ['.log', '.ini', '.err', '.out', '.js', '.txt']

commit ab05aed45613dbe363062686467d6b6166880b6e

covscan: do not require django on the client
... unless we are self-testing Covscan.  This commit fixes a regression
introduced in 81b122cf3de7efcaf276bf766b16d10ed4d5478d:

    Traceback (most recent call last):
      File "/usr/bin/covscan", line 48, in <module>
        sys.exit(main())
      File "/usr/bin/covscan", line 26, in main
        CovScanCommandContainer.register_module(covscan.commands, prefix="cmd_")
      File "/usr/lib/python2.7/site-packages/kobo/plugins.py", line 171, in register_module
        __import__(module.__name__, {}, {}, module_list)
      File "/usr/lib/python2.7/site-packages/covscan/commands/cmd_diff_build.py", line 9, in <module>
        from covscan.commands.shortcuts import verify_brew_koji_build, verify_mock, \
      File "/usr/lib/python2.7/site-packages/covscan/commands/shortcuts.py", line 8, in <module>
        from django.conf import settings
    ImportError: No module named django.conf

covscan/commands/shortcuts.py
@@ -5,7 +5,6 @@ import os
 import re
 
 from xmlrpclib import Fault
-from django.conf import settings
 
 
 __all__ = (
@@ -95,6 +94,7 @@ def upload_file(hub, srpm, target_dir, parser):
 
 
 if __name__ == '__main__':
+    from django.conf import settings
     print verify_brew_koji_build('scipy-0.12.1-1.el7', settings.BREW_URL, settings.KOJI_URL)
     print verify_brew_koji_build('xscipy-0.12.1-1.el7', settings.BREW_URL, settings.KOJI_URL)
     print verify_brew_koji_build('gnome-shell-3.10.2.1-3.fc20', settings.BREW_URL, settings.KOJI_URL)