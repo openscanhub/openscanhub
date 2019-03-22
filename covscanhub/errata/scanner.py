openscanhub/openscanhub

commit 1fce0c12ad4ab5fafef7a503c462b479f1d2cc60

support scanning of tarballs

covscanhub/errata/scanner.py
@@ -287,6 +287,8 @@ class AbstractClientScanScheduler(object):
             'keep_covdata': '--cov-keep-int-dir',
             'warning_level': '-w%s',
             # --cov-custom-model='%s' is not here because we need to upload file
+            'install_to_chroot': "--install='%s'",
+            'tarball_build_script': "--shell-cmd='%s'",
         }
         # client args
         cov_opts = self.options.get('args', [])
@@ -303,10 +305,11 @@ class AbstractClientScanScheduler(object):
                 cov_opts.append(cov_args[opt])
         for opt in self.options:
             if opt in csmock_args:
-                if opt == 'warning_level':
+                try:
                     csmock_opts.append(csmock_args[opt] % self.options[opt])
-                else:
-                    csmock_opts.append(csmock_args[opt])
+                except TypeError:
+                    # value might not need to be converted
+                    pass
         # client overrides via --csmock-args
         if self.client_csmock_args:
             csmock_opts.append(self.client_csmock_args)
@@ -346,7 +349,8 @@ class ClientScanScheduler(AbstractClientScanScheduler):
         # srpm
         self.build_nvr = self.options.get('brew_build', None)
         self.upload_id = self.options.get('upload_id', None)
-        check_srpm_response = check_srpm(self.upload_id, self.build_nvr, self.username)
+        self.is_tarball = bool(self.options.get("tarball_build_script", None))
+        check_srpm_response = check_srpm(self.upload_id, self.build_nvr, self.username, self.is_tarball)
         if check_srpm_response['type'] == 'build':
             self.build_kojibin = check_srpm_response['koji_bin']
         elif check_srpm_response['type'] == 'upload':
@@ -387,7 +391,8 @@ class ClientScanScheduler(AbstractClientScanScheduler):
     def prepare_args(self):
         """ prepare dicts -- arguments for task and scan """
         self.task_args['owner_name'] = self.username
-        self.task_args['label'] = self.build_nvr or self.srpm_name
+        input_pkg = self.build_nvr or self.srpm_name
+        self.task_args['label'] = input_pkg
         self.task_args['method'] = self.method
         self.task_args['comment'] = self.comment
         self.task_args['priority'] = self.priority or 10
@@ -401,6 +406,23 @@ class ClientScanScheduler(AbstractClientScanScheduler):
         else:
             self.task_args['args']['srpm_name'] = self.srpm_name
 
+        if input_pkg.endswith(".src.rpm"):
+            result_filename = os.path.basename(input_pkg)[:-8]
+        elif self.is_tarball:
+            f = os.path.basename(input_pkg)
+            if ".tar." in f:
+                result_filename = f.rsplit(".", 2)[0]
+            else:
+                result_filename = f.rsplit(".", 1)[0]
+        else:
+            raise RuntimeError("unknown input format of sources")
+        self.task_args['args']['result_filename'] = result_filename
+        # FIXME: ideally rewrite the code to stuff all input-related info to "source" (e.g. builds,
+        #        nvrs, srpm filenames etc.)
+        if self.is_tarball:
+            self.task_args['args']['source'] = {
+                "type": "tar",
+            }
         analyzer_opts = ClientAnalyzer.objects.get_opts(self.analyzer_models)
         analyzers_set = set(analyzer_opts['analyzers'] + self.profile_analyzers)
         analyzer_chain = ','.join(analyzers_set)

covscan/commands/cmd_diff_build.py
@@ -38,6 +38,7 @@ class Diff_Build(covscan.CovScanCommand):
         add_analyzers_option(self.parser)
         add_profile_option(self.parser)
         add_csmock_args_option(self.parser)
+
         add_keep_covdata_option(self.parser)
         add_comment_option(self.parser)
         add_task_id_file_option(self.parser)
@@ -48,6 +49,9 @@ class Diff_Build(covscan.CovScanCommand):
         add_all_option(self.parser)
         add_security_option(self.parser)
         add_custom_model_option(self.parser)
+
+        add_tarball_option(self.parser)
+        add_install_to_chroot_option(self.parser)
 
         self.parser.add_option(
             "-m",
@@ -113,21 +117,25 @@ exist." % self.results_store_file)
         profile = kwargs.pop('profile', None)
         csmock_args = kwargs.pop('csmock_args', None)
         cov_custom_model = kwargs.pop('cov_custom_model', None)
+        tarball_build_script = kwargs.pop('tarball_build_script', None)
+        packages_to_install = kwargs.pop('install_to_chroot', None)
 
         if len(args) != 1:
             self.parser.error("please specify exactly one SRPM")
-        self.srpm = args[0]
+        self.srpm = os.path.abspath(os.path.expanduser(args[0]))
 
         self.validate_results_store_file()
 
-        if not brew_build and not self.srpm.endswith(".src.rpm"):
-            self.parser.error("provided file doesn't appear to be a SRPM")
-
         if brew_build:
+            # get build from koji
             result = verify_brew_koji_build(self.srpm, self.conf['BREW_URL'],
                                             self.conf['KOJI_URL'])
             if result is not None:
                 self.parser.error(result)
+        elif tarball_build_script:
+            # we are analyzing tarball with build script
+            if not os.path.exists(self.srpm):
+                self.parser.error("Tarball does not exist.")
 
         if not config:
             config = local_conf.get_default_mockconfig()
@@ -202,6 +210,11 @@ is not even one in your user configuration file \
             upload_model_id, err_code, err_msg = upload_file(self.hub, cov_custom_model,
                                                        target_dir, self.parser)
             options["upload_model_id"] = upload_model_id
+
+        if packages_to_install:
+            options['install_to_chroot'] = packages_to_install
+        if tarball_build_script:
+            options['tarball_build_script'] = tarball_build_script
 
         task_id = self.submit_task(config, comment, options)
 

covscan/commands/cmd_version_diff_build.py
@@ -141,9 +141,6 @@ local file"
             self.parser.error("provided target file doesn't appear to be \
 a SRPM")
 
-        if base_srpm and not base_srpm.endswith(".src.rpm"):
-            self.parser.error("provided base file doesn't appear to be a SRPM")
-
         if brew_build:
             result = verify_brew_koji_build(brew_build, self.conf['BREW_URL'],
                                             self.conf['KOJI_URL'])

covscan/commands/common.py
@@ -194,3 +194,23 @@ def add_custom_model_option(parser):
     )
 
 
+def add_install_to_chroot_option(parser):
+    parser.add_option(
+        "--install",
+        dest="install_to_chroot",
+        action="store",
+        help="When scanning tarballs, you can install packages into chroot with this option ("
+             "usually devel packages)"
+    )
+
+
+def add_tarball_option(parser):
+    parser.add_option(
+        "--tarball-build-script",
+        dest="tarball_build_script",
+        action="store",
+        help="With this option covscan accepts path to tarball specified via first argument and "
+             "then the tarball will be scanned. "
+             "This option sets command which should build the package, usually this should be just "
+             "\"make\", in case of packages which doesn't need to be built, just pass \"true\".",
+    )

covscand/tasks/task_mock_build.py
@@ -28,6 +28,7 @@ class MockBuild(TaskBase):
         su_user = self.args.pop('su_user', None)
         custom_model_name = self.args.pop("custom_model_name", None)
         task_url = self.hub.client.task_url(self.task_id)
+        result_filename = self.args.pop("result_filename")
 
         cim = self.hub.worker.get_cim_arg(self.task_id)
         if cim:
@@ -58,6 +59,7 @@ class MockBuild(TaskBase):
                     url,
                     profile=mock_config,
                     additional_arguments=csmock_args,
+                    result_filename=result_filename,
                     su_user=su_user)
             else:
                 print >> sys.stderr, "No srpm specified"

covscanhub/errata/check.py
@@ -95,7 +95,7 @@ def check_analyzers(analyzers_chain, additional_analyzers=None):
     return ClientAnalyzer.objects.verify_in_bulk(a_list)
 
 
-def check_upload(upload_id, task_user):
+def check_upload(upload_id, task_user, is_tarball=False):
     """
     srpm was uploaded via FileUpload, lets fetch it and check it
 
@@ -111,11 +111,13 @@ def check_upload(upload_id, task_user):
 
     srpm_path = os.path.join(upload.target_dir, upload.name)
     srpm_name = upload.name
-    nvr = srpm_name.replace('.src.rpm', '')
+    nvr = ''
+    if not is_tarball:
+        nvr = srpm_name.replace('.src.rpm', '')
     return nvr, srpm_name, srpm_path
 
 
-def check_srpm(upload_id, build_nvr, task_user):
+def check_srpm(upload_id, build_nvr, task_user, is_tarball=False):
     if build_nvr:
         cb_response = check_build(build_nvr, check_additional=True)
         response = {
@@ -125,7 +127,7 @@ def check_srpm(upload_id, build_nvr, task_user):
             'koji_url': cb_response[1],
         }
     elif upload_id:
-        cu_response = check_upload(upload_id, task_user)
+        cu_response = check_upload(upload_id, task_user, is_tarball)
         response = {
             'type': 'upload',
             'nvr': cu_response[0],

covscanhub/service/csmock_parser.py
@@ -268,15 +268,16 @@ class CsmockRunner(object):
                 return None, retcode
 
     def analyze(self, analyzers, srpm_path, profile=None, su_user=None, additional_arguments=None,
-                use_sudo=False, **kwargs):
-        if not srpm_path.endswith('.src.rpm'):
-            raise RuntimeError("'srpm' path has to end with '.src.rpm'")
-        base_srpm = os.path.basename(srpm_path)[:-8]
+                use_sudo=False, result_filename=None, **kwargs):
         if self.tmpdir:
-            output_path = os.path.join(self.tmpdir, base_srpm + '.tar.xz')
+            output_path = os.path.join(self.tmpdir, result_filename + '.tar.xz')
         else:
-            output_path = os.path.join(os.getcwd(), base_srpm + '.tar.xz')
-        cmd = '-t %s -o %s' % (pipes.quote(analyzers), pipes.quote(output_path))
+            output_path = os.path.join(os.getcwd(), result_filename + '.tar.xz')
+        cmd = ""
+        if analyzers:
+            cmd += '-t %s' % (pipes.quote(analyzers))
+        if output_path:
+            cmd += ' -o %s' % (pipes.quote(output_path))
         if profile:
             cmd += ' -r %s' % pipes.quote(profile)
         if additional_arguments:
@@ -287,6 +288,7 @@ class CsmockRunner(object):
     def srpm_download_analyze(self, analyzers, srpm_name, srpm_url, profile=None,
                               su_user=None, additional_arguments=None, use_sudo=False, **kwargs):
         """ download srpm from remote location and analyze it"""
+        logger.debug("additional args = %s, kwargs = %s", additional_arguments, kwargs)
         if self.tmpdir:
             srpm_path = os.path.join(self.tmpdir, srpm_name)
         else:

commit 91bbbd3aaf40b7eb5feae9ab902cf5e9aced023a

covscanhub: do not silently ignore --keep-covdata
This fixes a regression from 1fce0c12ad4ab5fafef7a503c462b479f1d2cc60.
If the mapped option string does not contain any conversions, it should
be used as it is (instead of being silently ignored).

Closes #19

covscanhub/errata/scanner.py
@@ -308,8 +308,8 @@ class AbstractClientScanScheduler(object):
                 try:
                     csmock_opts.append(csmock_args[opt] % self.options[opt])
                 except TypeError:
-                    # value might not need to be converted
-                    pass
+                    # value does not need to be converted
+                    csmock_opts.append(csmock_args[opt])
         # client overrides via --csmock-args
         if self.client_csmock_args:
             csmock_opts.append(self.client_csmock_args)