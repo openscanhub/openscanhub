1484a6f2b2519a3642cda1a94a04dcb101e8d4e9
Isabella Basso do Amaral <idoamara@redhat.com>
2022-11-08 09:24:32 -0300

    delete unused script files
    Signed-off-by: Isabella Basso do Amaral <idoamara@redhat.com>

--- covscanhub/scripts/et_scanning_test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-./covscan-xmlrpc-client.py -b wget-1.11.4-4.el6 -t wget-1.12-1.8.el6
-./covscan-xmlrpc-client.py -b coreutils-8.4-5.el6 -t coreutils-8.4-19.el6
-./covscan-xmlrpc-client.py -b hardlink-1.0-9.el6 -t hardlink-1.0-10.el6
-./covscan-xmlrpc-client.py -b NEW_PACKAGE -t tar-1.23-11.el6
