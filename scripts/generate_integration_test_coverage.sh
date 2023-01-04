f7ca5f31165ffe4e3aff0cdd4d77da304a261924
Siteshwar Vashisht <svashisht@redhat.com>
2023-01-04 10:08:33 +0100

    Add integration tests for differential builds
    Add integration tests for `diff-build` and `version-diff-build` options
    for client.
    
    Resolves: https://gitlab.cee.redhat.com/covscan/covscan/-/issues/143

--- scripts/generate_integration_test_coverage.sh
+++ scripts/generate_integration_test_coverage.sh
@@ -47,14 +47,32 @@ set -e; set -o pipefail
 podman exec -it osh-client "${CLI_CMD[@]}" list-analyzers | grep gcc
 podman exec -it osh-client "${CLI_CMD[@]}" list-profiles | grep default
 podman exec -it osh-client "${CLI_CMD[@]}" list-mock-configs | grep fedora
-podman exec osh-client "${CLI_CMD[@]}" mock-build --config=fedora-36-x86_64 --brew-build units-2.21-4.fc36 | grep http://osh-hub:8000/task/
+podman exec osh-client "${CLI_CMD[@]}" mock-build --config=fedora-36-x86_64 --brew-build units-2.21-4.fc36 | grep http://osh-hub:8000/task/1
 [[ $(podman exec osh-client "${CLI_CMD[@]}" task-info 1 | wc -l) -gt 0 ]]
 podman exec -it osh-client "${CLI_CMD[@]}" download-results 1
 untar_output=$(tar xvf units*.tar.xz)
 untar_dir_name=$(echo $untar_output | cut -d' ' -f1)
 [[ -f "$untar_dir_name/scan-results.js" ]] && [[ -f "$untar_dir_name/scan-results.html" ]] && [[ -f "$untar_dir_name/scan.log" ]]
+rm -rf units*.tar.xz "$untar_dir_name"
 
 [[ $(podman exec osh-client "${CLI_CMD[@]}" find-tasks -p units) -eq 1 ]]
+
+podman exec osh-client "${CLI_CMD[@]}" diff-build --config=fedora-36-x86_64 --brew-build units-2.21-4.fc36 | grep http://osh-hub:8000/task/2
+podman exec -it osh-client "${CLI_CMD[@]}" download-results 2
+untar_output=$(tar xvf units*.tar.xz)
+untar_dir_name=$(echo $untar_output | cut -d' ' -f1)
+[[ -f "$untar_dir_name/scan-results.js" ]] && [[ -f "$untar_dir_name/scan-results.html" ]] && [[ -f "$untar_dir_name/scan.log" ]]
+rm -rf units*.tar.xz "$untar_dir_name"
+
+# `version-diff-build` needs worker to run in background
+podman exec -i osh-worker scripts/kill_worker.sh
+sed -i "s/RUN_TASKS_IN_FOREGROUND = 1/RUN_TASKS_IN_FOREGROUND = 0/g"  covscand/covscand-local.conf
+podman start osh-worker
+podman exec osh-client "${CLI_CMD[@]}" version-diff-build --config=fedora-36-x86_64 --brew-build units-2.21-4.fc36 --base-config=fedora-36-x86_64 --base-brew-build units-2.21-4.fc36 | grep http://osh-hub:8000/task/3
+podman exec -it osh-client "${CLI_CMD[@]}" download-results 3
+untar_output=$(tar xvf units*.tar.xz)
+untar_dir_name=$(echo $untar_output | cut -d' ' -f1)
+[[ -f "$untar_dir_name/scan-results.js" ]] && [[ -f "$untar_dir_name/scan-results.html" ]] && [[ -f "$untar_dir_name/scan.log" ]]
 
 podman exec osh-client /usr/bin/coverage-3.6 run --parallel-mode '--omit=*site-packages*,*kobo*,' covscanhub/scripts/covscan-xmlrpc-client.py --hub http://osh-hub:8000/xmlrpc/kerbauth/ --username=user --password=xxxxxx create-scan -b python-six-1.3.0-4.el7 -t python-six-1.9.0-2.el7 --et-scan-id=1 --release=RHEL-7.2.0 --owner=admin --advisory-id=1
 
