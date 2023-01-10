0643b492e8862cba3d3c7fe78212bf9ecfd6ecab
Isabella Basso do Amaral <idoamara@redhat.com>
2023-01-10 17:03:38 -0300

    containers/scripts/gen_integration_test_cov: use init-db script for deployment
    Signed-off-by: Isabella Basso do Amaral <idoamara@redhat.com>

--- containers/scripts/generate_integration_test_coverage.sh
+++ containers/scripts/generate_integration_test_coverage.sh
@@ -1,41 +1,10 @@
 #!/usr/bin/env bash
-set -x
 
 # shellcheck disable=1091
 source containers/scripts/utils.sh
 
-# Only get container IDs in quiet mode
-if [[ $(podman ps -a --filter label=io.podman.compose.project=covscan -q 2>/dev/null | wc -l) -gt 0 ]]; then
-    echo "One or more containers are already running under 'podman-compose'. Please use 'podman-compose down' to kill them."
-    exit 1
-fi
+FORCE=''
 
-# Remove stale coverage data
-rm -rf htmlcov .coverage
-
-# Clone kobo repository if it does not exist
-[[ -d kobo ]] || git clone --depth 1 https://github.com/release-engineering/kobo.git
-podman pull registry-proxy.engineering.redhat.com/rh-osbs/rhel8-postgresql-12
-podman build -f containers/hub/Dockerfile -t osh-hub .
-podman build -f containers/worker.Dockerfile -t osh-worker .
-podman build -f containers/client.Dockerfile -t osh-client .
-podman-compose up -d db osh-hub osh-worker osh-client
-
-while ! podman exec -it osh-hub pg_isready -h db; do sleep 0.5; done
-podman exec -it db psql -c 'ALTER USER covscanhub CREATEDB;'
-
-# Wait for hub to be ready before creating users
-wait_for_container 'HUB'
-
-podman exec -it osh-hub python3 covscanhub/manage.py shell<<EOF
-from django.contrib.auth import get_user_model
-User = get_user_model()
-User.objects.create_user('user', 'user@redhat.com', 'xxxxxx')
-User.objects.create_superuser('admin', 'user@redhat.com', 'xxxxxx')
-exit()
-EOF
-
-# Run client from local repository through python coverage
 CLI_CMD=(
     env
     COVSCAN_CONFIG_FILE=covscan/covscan-local.conf
@@ -44,52 +13,68 @@ CLI_CMD=(
     covscan/covscan
 )
 
-set -e; set -o pipefail
-# Only generate test coverage report for Covscan(OpenScanHub) project
-podman exec -it osh-client "${CLI_CMD[@]}" list-analyzers | grep gcc
-podman exec -it osh-client "${CLI_CMD[@]}" list-profiles | grep default
-podman exec -it osh-client "${CLI_CMD[@]}" list-mock-configs | grep fedora
-podman exec osh-client "${CLI_CMD[@]}" mock-build --config=fedora-36-x86_64 --brew-build units-2.21-4.fc36 | grep http://osh-hub:8000/task/1
-[[ $(podman exec osh-client "${CLI_CMD[@]}" task-info 1 | wc -l) -gt 0 ]]
-podman exec -it osh-client "${CLI_CMD[@]}" download-results 1
-untar_output=$(tar xvf units*.tar.xz)
-untar_dir_name=$(echo $untar_output | cut -d' ' -f1)
-[[ -f "$untar_dir_name/scan-results.js" ]] && [[ -f "$untar_dir_name/scan-results.html" ]] && [[ -f "$untar_dir_name/scan.log" ]]
-rm -rf units*.tar.xz "$untar_dir_name"
+main() {
+    set -ex
 
-[[ $(podman exec osh-client "${CLI_CMD[@]}" find-tasks -p units) -eq 1 ]]
+    ./containers/scripts/init-db.sh --deploy --full-dev --minimal "$FORCE"
 
-podman exec osh-client "${CLI_CMD[@]}" diff-build --config=fedora-36-x86_64 --brew-build units-2.21-4.fc36 | grep http://osh-hub:8000/task/2
-podman exec -it osh-client "${CLI_CMD[@]}" download-results 2
-untar_output=$(tar xvf units*.tar.xz)
-untar_dir_name=$(echo $untar_output | cut -d' ' -f1)
-[[ -f "$untar_dir_name/scan-results.js" ]] && [[ -f "$untar_dir_name/scan-results.html" ]] && [[ -f "$untar_dir_name/scan.log" ]]
-rm -rf units*.tar.xz "$untar_dir_name"
+    # Remove stale coverage data
+    rm -rf htmlcov .coverage
+    podman exec -it db psql -c 'ALTER USER covscanhub CREATEDB;'
 
-# `version-diff-build` needs worker to run in background
-podman exec -i osh-worker scripts/kill_worker.sh
-sed -i "s/RUN_TASKS_IN_FOREGROUND = 1/RUN_TASKS_IN_FOREGROUND = 0/g"  covscand/covscand-local.conf
-podman start osh-worker
-podman exec osh-client "${CLI_CMD[@]}" version-diff-build --config=fedora-36-x86_64 --brew-build units-2.21-4.fc36 --base-config=fedora-36-x86_64 --base-brew-build units-2.21-4.fc36 | grep http://osh-hub:8000/task/3
-podman exec -it osh-client "${CLI_CMD[@]}" download-results 3
-untar_output=$(tar xvf units*.tar.xz)
-untar_dir_name=$(echo $untar_output | cut -d' ' -f1)
-[[ -f "$untar_dir_name/scan-results.js" ]] && [[ -f "$untar_dir_name/scan-results.html" ]] && [[ -f "$untar_dir_name/scan.log" ]]
+    set -o pipefail
+    # Only generate test coverage report for Covscan(OpenScanHub) project
+    podman exec -it osh-client "${CLI_CMD[@]}" list-analyzers | grep gcc
+    podman exec -it osh-client "${CLI_CMD[@]}" list-profiles | grep default
+    podman exec -it osh-client "${CLI_CMD[@]}" list-mock-configs | grep fedora
+    podman exec osh-client "${CLI_CMD[@]}" mock-build --config=fedora-36-x86_64 --brew-build units-2.21-4.fc36 | grep http://osh-hub:8000/task/1
+    [[ $(podman exec osh-client "${CLI_CMD[@]}" task-info 1 | wc -l) -gt 0 ]]
+    podman exec -it osh-client "${CLI_CMD[@]}" download-results 1
+    untar_output=$(tar xvf units*.tar.xz)
+    untar_dir_name=$(echo $untar_output | cut -d' ' -f1)
+    [[ -f "$untar_dir_name/scan-results.js" ]] && [[ -f "$untar_dir_name/scan-results.html" ]] && [[ -f "$untar_dir_name/scan.log" ]]
+    rm -rf units*.tar.xz "$untar_dir_name"
 
-podman exec osh-client /usr/bin/coverage-3.6 run --parallel-mode '--omit=*site-packages*,*kobo*,' covscanhub/scripts/covscan-xmlrpc-client.py --hub http://osh-hub:8000/xmlrpc/kerbauth/ --username=user --password=xxxxxx create-scan -b python-six-1.3.0-4.el7 -t python-six-1.9.0-2.el7 --et-scan-id=1 --release=RHEL-7.2.0 --owner=admin --advisory-id=1
+    [[ $(podman exec osh-client "${CLI_CMD[@]}" find-tasks -p units) -eq 1 ]]
 
-set +e; set +o pipefail
+    podman exec osh-client "${CLI_CMD[@]}" diff-build --config=fedora-36-x86_64 --brew-build units-2.21-4.fc36 | grep http://osh-hub:8000/task/2
+    podman exec -it osh-client "${CLI_CMD[@]}" download-results 2
+    untar_output=$(tar xvf units*.tar.xz)
+    untar_dir_name=$(echo $untar_output | cut -d' ' -f1)
+    [[ -f "$untar_dir_name/scan-results.js" ]] && [[ -f "$untar_dir_name/scan-results.html" ]] && [[ -f "$untar_dir_name/scan.log" ]]
+    rm -rf units*.tar.xz "$untar_dir_name"
 
-# We have to kill django server and worker to generate coverage files
-podman exec -i osh-worker scripts/kill_worker.sh
-podman exec -i osh-hub scripts/kill_django_server.sh
+    # `version-diff-build` needs worker to run in background
+    podman exec -i osh-worker scripts/kill_worker.sh
+    sed -i "s/RUN_TASKS_IN_FOREGROUND = 1/RUN_TASKS_IN_FOREGROUND = 0/g"  covscand/covscand-local.conf
+    podman start osh-worker
+    podman exec osh-client "${CLI_CMD[@]}" version-diff-build --config=fedora-36-x86_64 --brew-build units-2.21-4.fc36 --base-config=fedora-36-x86_64 --base-brew-build units-2.21-4.fc36 | grep http://osh-hub:8000/task/3
+    podman exec -it osh-client "${CLI_CMD[@]}" download-results 3
+    untar_output=$(tar xvf units*.tar.xz)
+    untar_dir_name=$(echo $untar_output | cut -d' ' -f1)
+    [[ -f "$untar_dir_name/scan-results.js" ]] && [[ -f "$untar_dir_name/scan-results.html" ]] && [[ -f "$untar_dir_name/scan.log" ]]
 
-# Combine coverage report for hub, worker and client
-podman exec -it osh-client /usr/bin/coverage-3.6 combine
+    podman exec osh-client /usr/bin/coverage-3.6 run --parallel-mode '--omit=*site-packages*,*kobo*,' covscanhub/scripts/covscan-xmlrpc-client.py --hub http://osh-hub:8000/xmlrpc/kerbauth/ --username=user --password=xxxxxx create-scan -b python-six-1.3.0-4.el7 -t python-six-1.9.0-2.el7 --et-scan-id=1 --release=RHEL-7.2.0 --owner=admin --advisory-id=1
 
-# Convert test coverage to html
-podman exec -it osh-client /usr/bin/coverage-3.6 html
+    set +e; set +o pipefail
 
-# Open the coverage report under your favourite browser
-echo "Coverage report generated in 'htmlcov' directory."
-echo "Use 'xdg-open htmlcov/index.html' command to open it."
+    # We have to kill django server and worker to generate coverage files
+    podman exec -i osh-worker scripts/kill_worker.sh
+    podman exec -i osh-hub scripts/kill_django_server.sh
+
+    # Combine coverage report for hub, worker and client
+    podman exec -it osh-client /usr/bin/coverage-3.6 combine
+
+    # Convert test coverage to html
+    podman exec -it osh-client /usr/bin/coverage-3.6 html
+
+    # Open the coverage report under your favourite browser
+    echo "Coverage report generated in 'htmlcov' directory."
+    echo "Use 'xdg-open htmlcov/index.html' command to open it."
+}
+
+if [ "$1" = --force ]; then
+    FORCE='--force'
+fi
+
+main
