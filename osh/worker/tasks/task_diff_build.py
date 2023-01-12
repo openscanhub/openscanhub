05f054761162e7cbf7e15cea72c62fb887cedeac
Siteshwar Vashisht <svashisht@redhat.com>
2023-01-12 15:15:44 +0100

    Fix warnings reported by Flake8

--- osh/worker/tasks/task_diff_build.py
+++ osh/worker/tasks/task_diff_build.py
@@ -2,15 +2,16 @@
 
 
 from __future__ import absolute_import
+
 from .task_mock_build import MockBuild
 
 
 class DiffBuild(MockBuild):
     enabled = True
 
-    arches = ["noarch"]    # list of supported architectures
-    channels = ["default"] # list of channels
-    exclusive = False      # leave False here unless you really know what you're doing
-    foreground = False     # if True the task is not forked and runs in the worker process (no matter you run worker without -f)
+    arches = ["noarch"]     # list of supported architectures
+    channels = ["default"]  # list of channels
+    exclusive = False       # leave False here unless you really know what you're doing
+    foreground = False      # if True the task is not forked and runs in the worker process (no matter you run worker without -f)
     priority = 10
     weight = 1.0
