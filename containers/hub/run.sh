openscanhub/openscanhub

commit 1f757da1871625c566dbbf7379394d831474b2ee

covscanhub/static: drop from the git repo
As pointed out by Isabella, the `static` directory should actually
be created by Django itself during installation of the application(s).

Some of the files are copied from the `media` directory and other
files are copied from Django/Kobo.  Maintaining an outdated copy
of the `static` directory in the git repo is not useful.

Fixes: https://gitlab.cee.redhat.com/covscan/covscan/-/issues/145
Closes: https://gitlab.cee.redhat.com/covscan/covscan/-/merge_requests/159

containers/hub/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# collect static files from Django itself
+python3.6 covscanhub/manage.py collectstatic
+
 for _ in $(seq 100); do
     pg_isready -h db && break
     sleep 0.5

covscan.spec
@@ -6,10 +6,19 @@ Summary:        Coverity scan scheduler
 Source:         %{name}-%{version}.tar.bz2
 BuildArch:      noarch
 
+BuildRequires:  koji
+BuildRequires:  python3-csdiff
 BuildRequires:  python3-devel
+BuildRequires:  python3-django
+BuildRequires:  python3-django-debug-toolbar
+BuildRequires:  python3-kobo-client
+BuildRequires:  python3-kobo-django
+BuildRequires:  python3-kobo-hub
+BuildRequires:  python3-kobo-rpmlib
+BuildRequires:  python3-psycopg2
+BuildRequires:  python3-qpid-proton
 BuildRequires:  python3-setuptools
 BuildRequires:  python3-six
-BuildRequires:  python3-kobo-client
 BuildRequires:  systemd-rpm-macros
 
 %description
@@ -80,7 +89,7 @@ Requires: file
 Requires: python3-bugzilla
 Requires: python3-csdiff
 
-Requires: python3-django-debug-toolbar > 1.0
+Requires: python3-django-debug-toolbar
 
 Requires(post): /usr/bin/pg_isready
 
@@ -113,6 +122,10 @@ done;done)
 %setup -q
 
 %build
+
+# collect static files from Django itself
+PYTHONPATH=. covscanhub/manage.py collectstatic
+
 %py3_build
 
 

commit c78d42ae27baf673ff9605add865e3ec63057b63

hub: fix `collectstatic` call
`collectstatic` should be called with the `--noinput` flag on scripts as
the user isn't able to type in a response to the prompt.

Fixes: 1f757da1 ("covscanhub/static: drop from the git repo")

Signed-off-by: Isabella Basso do Amaral <idoamara@redhat.com>

containers/hub/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # collect static files from Django itself
-python3.6 covscanhub/manage.py collectstatic
+python3.6 covscanhub/manage.py collectstatic --noinput
 
 for _ in $(seq 100); do
     pg_isready -h db && break

covscan.spec
@@ -124,7 +124,7 @@ done;done)
 %build
 
 # collect static files from Django itself
-PYTHONPATH=. covscanhub/manage.py collectstatic
+PYTHONPATH=. covscanhub/manage.py collectstatic --noinput
 
 %py3_build
 