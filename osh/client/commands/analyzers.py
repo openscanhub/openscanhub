774adae720817c4a917202af1b7180278ef7b3f0
Lukáš Zaoral <lzaoral@redhat.com>
2023-02-20 10:44:24 +0100

    client/commands/analyzers: remove Python 2 remnants

--- osh/client/commands/analyzers.py
+++ osh/client/commands/analyzers.py
@@ -1,12 +1,4 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import
-
-import six
-
-
 def check_analyzers(proxy, analyzers_list):
     result = proxy.scan.check_analyzers(analyzers_list)
-
-    if isinstance(result, six.string_types):
-        raise RuntimeError(str(result))
+    if isinstance(result, str):
+        raise RuntimeError(result)
