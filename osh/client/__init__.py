1d9c6bd00577737be9697d04e7abf7274e595f18
Lukáš Zaoral <lzaoral@redhat.com>
2023-02-22 13:56:44 +0100

    client/__init__: use print for file writing
    We already use it in few places for sys.stderr so let's be consistent.

--- osh/client/__init__.py
+++ osh/client/__init__.py
@@ -5,4 +5,4 @@ class OshCommand(ClientCommand):
     def write_task_id_file(self, task_id, filename=None):
         if filename is not None:
             with open(filename, "w") as f:
-                f.write(f"{task_id}\n")
+                print(task_id, file=f)
