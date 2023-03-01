eb75aceaacb3792aa7761371e6f48b018be4af71
Jiri Kucera <jkucera@redhat.com>
2023-03-01 12:01:56 +0100

    covscanhub/scan: Refactor get_compare_title
    `get_compare_title` from `covscanhub.scan.compare` is seen as too
    complex by linters and it takes a while to realize how it works
    from reading its source. Moreover it crashes if NVR is ill-formed.
    
    This commit refactors it in the following fashion:
    * the original function has been analyzed first
      - it consists of two nested loops
      - the outer loop iterates over name, version, and release (so
        the upper bound for the number of iterations is 3)
      - the inner loop iterates over dot-separated parts of name or
        version or release
    * the inner loop has been put into its own function `compare_nvr_parts`
      alongside with introducing auxiliary functions `parse_nvr`, `mark`,
      `mark_other`, and `mark_base`
    * the outer loop has been unrolled
    
    All newly introduced functions are provided with docstrings describing
    their interface and purpose. The dead code has been also removed.

--- covscanhub/scan/compare.py
+++ covscanhub/scan/compare.py
@@ -1,265 +1,198 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
+import itertools
 import re
-import difflib
 
 from django.utils.safestring import mark_safe
-
 
 __all__ = (
     'get_compare_title',
 )
 
+CSS_CLASS_OTHER = "light_green_font"
+CSS_CLASS_BASE = "red_font"
 
-def display_diff(css_class, diff_list):
+
+def parse_nvr(nvr):
     """
-    suround every item of diff_list with <span> using provided CSS class
+    Split NVR to name, version, and release.
+
+    :param nvr: The NVR (name-version-release).
+    :type nvr:
+
+    :return: the `tuple` containing name, version, and release string
+    :rtype: tuple
+
+    In case NVR is ill-formed:
+    * if it is in a form of ``X-Y``, return ``(X, Y, "")``;
+    * otherwise, return ``(nvr, "", "")``.
     """
-    if not diff_list:
-        return ''
-    response = ''
-    for d in diff_list:
-        response += '<span class="%s">' % css_class + d + '</span>' + '.'
-    return response[:-1]
+    nvr_parts = re.match("(.*)-(.*)-(.*)", nvr)
+    if nvr_parts:
+        return nvr_parts.group(1), nvr_parts.group(2), nvr_parts.group(3)
+    # One of the NVR components is missing:
+    nvr_parts = re.match("(.*)-(.*)", nvr)
+    if nvr_parts:
+        return nvr_parts.group(1), nvr_parts.group(2), ""
+    # Two of the NVR components are missing:
+    return nvr, "", ""
+
+
+def mark(content, css_class):
+    """
+    Surround *content* with ``<span>`` using *css_class*.
+
+    :param content: The content
+    :type content: str
+    :param css_class: The CSS class used in ``<span>``
+    :type css_class: str
+
+    :return: *content* surrounded with ``<span>`` styled with *css_class*
+    :rtype: str
+    """
+    return f'<span class="{css_class}">{content}</span>'
+
+
+def mark_other(content):
+    """
+    Render *content* in light green color using ``<span>``.
+
+    :param content: The content
+    :type content: str
+
+    :return: *content* in light green color as a HTML string
+    :rtype: str
+    """
+    return mark(content, CSS_CLASS_OTHER)
+
+
+def mark_base(content):
+    """
+    Render *content* in red color using ``<span>``.
+
+    :param content: The content
+    :type content: str
+
+    :return: *content* in red color as a HTML string
+    :rtype: str
+    """
+    return mark(content, CSS_CLASS_BASE)
+
+
+def compare_nvr_parts(other, base, prev_differ=False):
+    """
+    Compare two names, versions, or releases.
+
+    :param other: Name, version, or release as a list of its parts to be
+        compared against *base*
+    :type other: list
+    :param base: Name, version, or release as a list of its parts
+    :type base: list
+    :param prev_differ: `True` if the previous comparison of name, version, or
+        release yielded a difference
+    :type prev_differ: bool
+
+    :return: the `tuple` containing a `bool` flag signaling whether *other* and
+      *base* are different, a string with marked different parts of *other*,
+      and a string with marked different parts of *base*
+    :rtype: tuple
+
+    If *prev_differ* is `True`, *base* and *other* are considered different and
+    their parts are marked accordingly. Otherwise, *base* and *other* are
+    considered different if and only if they are different in some *n*th part.
+    In this case, all parts starting from *n*th part to the last part are
+    marked different. Empty parts are not marked.
+
+    Examples: Suppose that ``mark_other(x)`` yields ``f"({x})"`` and
+    ``mark_base(x)`` yields ``f"[{x}]"``. Then
+    * ``compare_nvr_parts(["1", "2", "3"], ["1", "2", "3"], True)`` yields
+      ``(True, "(1).(2).(3)", "[1].[2].[3]")``;
+    * ``compare_nvr_parts(["1", "2", "3"], ["1", "1", "3"], False)`` yields
+      ``(True, "1.(2).(3)", "1.[1].[3]")``;
+    * ``compare_nvr_parts(["1", "2"], ["1", "2", "3"], False)`` yields
+      ``(True, "1.2", "1.2.[3]")``.
+    """
+    differ = prev_differ
+    diff_other = []
+    diff_base = []
+    for elm1, elm2 in itertools.zip_longest(other, base, fillvalue=""):
+        differ = differ or elm1 != elm2
+        if elm1:
+            diff_other.append(differ and mark_other(elm1) or elm1)
+        if elm2:
+            diff_base.append(differ and mark_base(elm2) or elm2)
+    return differ, ".".join(diff_other), ".".join(diff_base)
 
 
 def get_compare_title(nvr, base_nvr):
     """
-    compare two NVRs, mark different parts with <span>
+    Compare two NVRs, mark different parts with ``<span>``.
+
+    :param nvr: The name-version-release to be compared against base
+    :type nvr: str
+    :param base_nvr: The base name-version-release
+    :type base_nvr: str
+
+    :return: the comparison summary
+    :rtype: str
+
+    A name-version-release string, NVR for short, is a string matching the
+    following format::
+
+        <name> "-" <version> "-" <release>
+
+    where ``<version>`` and ``<release>`` must not contain a dash (``-``),
+    dashes in ``<name>`` are allowed. Additionally, ``<version>`` and
+    ``<release>`` are composed from parts separated by a dot (``.``).
+
+    Two NVRs are compared part-wise from the left to the right as follows:
+
+    #. If ``<name>``s differ, everything in NVR is marked as different.
+    #. Otherwise, if ``<version>``s differ in some part, the part and the
+       remaining parts are marked as different.
+    #. Otherwise, if ``<release>``s differ in some part, the part and the
+       remaining parts are marked as different.
+    #. Otherwise, NVRs are equal.
+
+    For more clarity, let demonstrate it on several examples. For simplicity,
+    ``<span class="light_green_font">X</span>`` and
+    ``<span class="red_font">Y</span>`` will be abbreviated ``(X)`` and
+    ``[Y]``, respectively.
+
+    Examples:
+    #. A comparison of ``foo-1.2.3-4.el8`` and ``bar-1.2.3-4.el8`` yields
+       ``(foo)-(1).(2).(3)-(4).(el8) compared to [bar]-[1].[2].[3]-[4].[el8]``.
+    #. A comparison of ``foo-2.2.3-4.el8`` and ``foo-1.2.3-4.el8`` yields
+       ``foo-(2).(2).(3)-(4).(el8) compared to foo-[1].[2].[3]-[4].[el8]``.
+    #. A comparison of ``foo-1.2.3-4.el8`` and ``foo-1.3.3-4.el8`` yields
+       ``foo-1.(2).(3)-(4).(el8) compared to foo-1.[3].[3]-[4].[el8]``.
+    #. A comparison of ``foo-1.2.3-4.el8`` and ``foo-1.2.2-4.el8`` yields
+       ``foo-1.2.(3)-(4).(el8) compared to foo-1.2.[2]-[4].[el8]``.
+    #. A comparison of ``foo-1.2.3-4.el8`` and ``foo-1.2.3-4.el9`` yields
+       ``foo-1.2.3-4.(el8) compared to foo-1.2.3-4.[el8]``.
+    #. A comparison of ``foo-1.2.3-4.el8`` and ``foo-1.2-4.el8`` yields
+       ``foo-1.2.(3)-(4).(el8) compared to foo-1.2-[4].[el8]``.
     """
-    re_nvr = re.match("(.*)-(.*)-(.*)", nvr)
-    re_base = re.match("(.*)-(.*)-(.*)", base_nvr)
-    outer_index = 1  # firstly compare version, then release
-    inner_index = 0  # hop over all version numbers
-    result_nvr = ''
-    result_base = ''
-    diff_found = False
-    while True:
-        try:
-            nvr_numbers = re_nvr.group(outer_index).split('.')
-        except IndexError:
-            break
-        base_numbers = re_base.group(outer_index).split('.')
-        # mark rest if diff was already found
-        if diff_found:
-            result_nvr += display_diff('light_green_font', nvr_numbers)
-            result_base += display_diff('red_font', base_numbers)
-        else:
-            while True:
-                try:
-                    # compare current part
-                    condition = nvr_numbers[inner_index] == \
-                        base_numbers[inner_index]
-                except IndexError:
-                    # we are out of index:
-                    # - this might be due to fact that we processed last item
-                    # - or that lists have different count of items
-                    if len(nvr_numbers) != len(base_numbers):
-                        diff_found = True
-                        try:
-                            result_nvr += display_diff(
-                                'light_green_font',
-                                nvr_numbers[inner_index:len(nvr_numbers)])
-                        except IndexError:
-                            pass
-                        try:
-                            result_base += display_diff(
-                                'red_font',
-                                base_numbers[inner_index:len(base_numbers)])
-                        except IndexError:
-                            pass
-                    if result_nvr.endswith('.'):
-                        result_nvr = result_nvr[:-1]
-                    if result_base.endswith('.'):
-                        result_base = result_base[:-1]
-                    inner_index = 0
-                    break
-                if condition:
-                    # condition holds, diff wasn't found yet
-                    result_nvr += nvr_numbers[inner_index] + '.'
-                    result_base += base_numbers[inner_index] + '.'
-                else:
-                    # we have found diff, mark rest
-                    diff_found = True
-                    result_nvr += display_diff('light_green_font',
-                                               nvr_numbers[inner_index:],)
-                    result_base += display_diff('red_font',
-                                                base_numbers[inner_index:])
-                    break
-                inner_index += 1
-        result_nvr += '-'
-        result_base += '-'
-        outer_index += 1
-    if result_nvr.endswith('-'):
-        result_nvr = result_nvr[:-1]
-    if result_base.endswith('-'):
-        result_base = result_base[:-1]
-    return mark_safe(result_nvr + ' compared to ' + result_base)
+    name, version, release = parse_nvr(nvr)
+    base_name, base_version, base_release = parse_nvr(base_nvr)
 
+    differ, diff_name_other, diff_name_base = compare_nvr_parts(
+        [name], [base_name]
+    )
+    differ, diff_version_other, diff_version_base = compare_nvr_parts(
+        version.split("."), base_version.split("."), differ
+    )
+    _, diff_release_other, diff_release_base = compare_nvr_parts(
+        release.split("."), base_release.split("."), differ
+    )
 
-def get_compare_title_re(nvr, base_nvr):
-    """
-    Different implementation of comparison using re
-    """
-    re_nvr = re.match("(.*)-(.*)-(.*)", nvr)
-    re_base = re.match("(.*)-(.*)-(.*)", base_nvr)
-    outer_index = 1
-    inner_index = 0
-    result_nvr = ''
-    result_base = ''
-    while True:
-        try:
-            nvr_numbers = re_nvr.group(outer_index).split('.')
-        except IndexError:
-            break
-        base_numbers = re_base.group(outer_index).split('.')
-        print(nvr_numbers, base_numbers)
-        while True:
-            try:
-                condition = nvr_numbers[inner_index] == \
-                    base_numbers[inner_index]
-            except IndexError:
-                if max(len(nvr_numbers), len(base_numbers)) != inner_index:
-                    try:
-                        result_nvr += '.'.join(nvr_numbers[inner_index:len(nvr_numbers)])
-                    except IndexError:
-                        pass
-                    try:
-                        result_base += '.'.join(base_numbers[inner_index:len(base_numbers)])
-                    except IndexError:
-                        pass
-                if result_nvr.endswith('.'): result_nvr = result_nvr[:-1]
-                if result_base.endswith('.'): result_base = result_base[:-1]
-                inner_index = 0
-                break
-            if condition:
-                result_nvr += nvr_numbers[inner_index] + '.'
-                result_base += base_numbers[inner_index] + '.'
-            else:
-                result_nvr += '<span class="result_target_nvr">%s</span>.'\
-                    % nvr_numbers[inner_index]
-                result_base += '<span class="result_base_nvr">%s</span>.'\
-                    % base_numbers[inner_index]
-            inner_index += 1
-        result_nvr += '-'
-        result_base += '-'
-        outer_index += 1
-    if result_nvr.endswith('-'): result_nvr = result_nvr[:-1]
-    if result_base.endswith('-'): result_base = result_base[:-1]
-    return mark_safe(result_nvr + ' compared to ' + result_base)
+    # Remove empty strings that were produced where some of NVR components are
+    # missing:
+    diff_other = list(
+        filter(None, [diff_name_other, diff_version_other, diff_release_other])
+    )
+    diff_base = list(
+        filter(None, [diff_name_base, diff_version_base, diff_release_base])
+    )
 
-
-def get_compare_title_difflib(nvr, base_nvr):
-    """
-    implementation that uses difflib
-    """
-    blocks = difflib.SequenceMatcher(a=nvr, b=base_nvr,
-                                     autojunk=False).get_matching_blocks()
-    print(nvr, base_nvr)
-    print(blocks)
-    i = 0
-    offset = 0
-    result_nvr = ''
-    result_base = ''
-    diff_active = False
-
-    while True:
-        if blocks[i][2] == 0:
-            if offset != max(len(nvr), len(base_nvr)):
-                result_nvr += nvr[offset:len(nvr)]
-                result_base += base_nvr[offset:len(base_nvr)]
-            if diff_active:
-                result_nvr += '</span>'
-                result_base += '</span>'
-            break
-
-        if diff_active:
-            result_nvr += nvr[offset:blocks[i][0]] + '</span>'
-            result_base += base_nvr[offset:blocks[i][1]] + '</span>'
-            offset += min(blocks[i][0]-offset, blocks[i][1]-offset)
-        else:
-            result_nvr += nvr[blocks[i][0]:blocks[i][2]+offset]
-            result_base += base_nvr[blocks[i][1]:blocks[i][2]+offset]
-            if blocks[i+1][2] != 0:
-                result_nvr += '<span class="result_target_nvr">'
-                result_base += '<span class="result_base_nvr">'
-            offset += blocks[i][2]
-            i += 1
-        diff_active ^= True  # alternate
-
-    return mark_safe(result_nvr + ' compared to ' + result_base)
-
-
-def get_display_diff_sequences(seq1, seq2):
-    blocks = difflib.SequenceMatcher(a=seq1, b=seq2,
-                                     autojunk=True).get_matching_blocks()
-    i = 0
-    seq1_offset = 0
-    seq2_offset = 0
-    result_seq1 = ''
-    result_seq2 = ''
-    if blocks[0][0] != 0 or blocks[0][1] != 0:
-        diff_active = True
-    else:
-        diff_active = False
-    while True:
-        if diff_active:
-            result_seq1 += display_diff('result_target_nvr', seq1[seq1_offset:blocks[i][0]])
-            result_seq2 += display_diff('result_base_nvr', seq2[seq2_offset:blocks[i][1]])
-            seq1_offset += blocks[i][0]
-            seq2_offset += blocks[i][1]
-        else:
-            result_seq1 += '.'.join(seq1[blocks[i][0]:blocks[i][2] + seq1_offset]) + '.'
-            result_seq2 += '.'.join(seq2[blocks[i][1]:blocks[i][2] + seq2_offset]) + '.'
-
-            seq1_offset += blocks[i][2]
-            seq2_offset += blocks[i][2]
-            i += 1
-        diff_active ^= True
-        if blocks[i][2] == 0:
-            if diff_active:
-                if seq1_offset < len(seq1):
-                    result_seq1 += display_diff('result_target_nvr',
-                                                seq1[seq1_offset:len(seq1)])
-                if seq2_offset < len(seq2):
-                    result_seq2 += display_diff('result_base_nvr',
-                                                seq2[seq2_offset:len(seq2)])
-            else:
-                if seq1_offset < len(seq1):
-                    result_seq1 += '.'.join(seq1[seq1_offset:len(seq1)])
-                if seq2_offset < len(seq2):
-                    result_seq2 += '.'.join(seq2[seq2_offset:len(seq2)])
-            break
-    if result_seq1.endswith('.'):
-        result_seq1 = result_seq1[:-1]
-    if result_seq2.endswith('.'):
-        result_seq2 = result_seq2[:-1]
-    return result_seq1, result_seq2
-
-
-def get_compare_title_re2(nvr, base_nvr):
-    """
-    yet another implementation
-    """
-    re_nvr = re.match("(.*)-(.*)-(.*)", nvr)
-    re_base = re.match("(.*)-(.*)-(.*)", base_nvr)
-    outer_index = 1
-    result_nvr = ''
-    result_base = ''
-    while True:
-        try:
-            nvr_numbers = re_nvr.group(outer_index).split('.')
-        except IndexError:
-            break
-        base_numbers = re_base.group(outer_index).split('.')
-        tmp_nvr, tmp_base = get_display_diff_sequences(nvr_numbers,
-                                                       base_numbers)
-        result_nvr += tmp_nvr + '-'
-        result_base += tmp_base + '-'
-        outer_index += 1
-    if result_nvr.endswith('-'):
-        result_nvr = result_nvr[:-1]
-    if result_base.endswith('-'):
-        result_base = result_base[:-1]
-    return mark_safe(result_nvr + ' compared to ' + result_base)
+    return mark_safe(f'{"-".join(diff_other)} compared to {"-".join(diff_base)}')
