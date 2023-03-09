54883e21fc24ba8cd4d4448c228754794d051cea
Siteshwar Vashisht <svashisht@redhat.com>
2023-03-09 17:35:43 +0100

    Fix path to bash completions
    This is a follow up on commit 79a978d9f1fbb723f9f96e000d92b8aaad6c64a9
    
    Path to bash completions should be
    `osh/client/bash_completion.d/osh-cli.bash` and not
    `osh/cli/bash_completion.d/osh-cli.bash`.
    
    Related: https://gitlab.cee.redhat.com/covscan/covscan/-/issues/161

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ data_files = {
         "files/etc/systemd/system/covscand.service",
     ],
     "/etc/bash_completion.d": [
-        "osh/cli/bash_completion.d/osh-cli.bash",
+        "osh/client/bash_completion.d/osh-cli.bash",
     ],
     "/usr/bin": [
         "osh/client/osh-cli",
