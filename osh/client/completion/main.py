a09a8454614d707c8f0ad1470ef3d6ea298c21fc
Lukáš Zaoral <lzaoral@redhat.com>
2023-03-20 13:09:59 +0100

    client: restructure the completion code
    ... so that it can be easily extended with new functionality.  The cache
    reading and writing, hub connection and the fetching of data are now
    separate functions.

--- osh/client/completion/main.py
+++ osh/client/completion/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 import argparse
-import datetime
 import os
 import pickle
+from datetime import datetime, timedelta
 
 from kobo.client import HubProxy
 
@@ -12,66 +12,83 @@ from osh.common.conf import get_config_dict
 CACHE_PATH_PREFIX = '.cache/osh'
 
 
-def get_can_path():
+def get_can_path(action):
     """
     Return path to can with pickles
     """
     cache_path = os.path.join(os.path.expanduser('~'), CACHE_PATH_PREFIX)
     os.makedirs(cache_path, exist_ok=True)
-    return os.path.join(cache_path, 'bash_compl.pickle')
+    return os.path.join(cache_path, f'bash_compl_{action}.pickle')
 
 
-def get_configs_from_hub():
+def connect_to_hub():
     """
-    Return enabled mockconfigs from hub
+    Return hub proxy object
     """
     conf = get_config_dict(config_env="OSH_CLIENT_CONFIG_FILE",
                            config_default="/etc/osh/client.conf")
     if not conf:
-        return []
+        return None
 
-    hub = HubProxy(conf)
+    return HubProxy(conf)
+
+
+def fetch_mock_configs(hub):
+    """
+    Return enabled mock configs from hub
+    """
     return [x['name'] for x in hub.mock_config.all() if x['enabled']]
 
 
-def write_configs():
+def load_from_cache(action):
     """
-    write configs which were retieved from hub to pickle can
+    Try to load up to date data from the cache
     """
-    can_path = get_can_path()
-    configs = get_configs_from_hub()
-    with open(can_path, 'wb') as fd:
-        pickle.dump(configs, fd)
-    return configs
+    can_path = get_can_path(action)
 
-
-def list_enabled_mock_configs():
-    """
-    this function should be called from outside world
-    """
     try:
-        with open(get_can_path(), 'rb') as can:
-            can_time = datetime.datetime.fromtimestamp(
-                os.path.getmtime(get_can_path()))
-
-            if can_time + datetime.timedelta(minutes=5) > datetime.datetime.now():
-                enabled_configs = pickle.load(can)
-            else:
-                enabled_configs = write_configs()
+        with open(can_path, 'rb') as can:
+            can_time = datetime.fromtimestamp(os.path.getmtime(can_path))
+            if can_time + timedelta(minutes=5) > datetime.now():
+                return pickle.load(can)
     except OSError:
-        enabled_configs = write_configs()
+        pass
 
-    print(*enabled_configs)
+
+def fetch_options(action):
+    opts = []
+
+    # get the options
+    hub = connect_to_hub()
+    if hub is not None:
+        opts = ACTIONS[action](hub)
+
+    # store them to cache
+    with open(get_can_path(action), 'wb') as can:
+        pickle.dump(opts, can)
+
+    return opts
 
 
 def main(args):
-    if 'mock-configs' == args.action:
-        list_enabled_mock_configs()
+    # try cache first
+    opts = load_from_cache(args.action)
+
+    # otherwise, fetch it from the server
+    if opts is None:
+        opts = fetch_options(args.action)
+
+    print(*opts)
+
+
+ACTIONS = {
+    'mock-configs': fetch_mock_configs,
+}
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('action', choices=['mock-configs'],
-                        help='select what should be completed')
+    parser.add_argument('action', help='select what should be completed',
+                        choices=ACTIONS.keys())
 
     main(parser.parse_args())
