00c568310141cf742369a6aa16d600dab5990d74
Isabella Basso do Amaral <idoamara@redhat.com>
2023-04-11 08:59:55 -0300

    hub: urls: linter fixes
    Signed-off-by: Isabella Basso do Amaral <idoamara@redhat.com>

--- osh/hub/urls.py
+++ osh/hub/urls.py
@@ -1,5 +1,5 @@
-from django.urls import include, path
 from django.contrib import admin
+from django.urls import include, path
 from django.views.generic.base import TemplateView
 
 admin.autodiscover()
