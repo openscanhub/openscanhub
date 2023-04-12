openscanhub/openscanhub

commit e027357d8a2c83add2393044f4f068486d152127

hub: drop django.conf.urls
... as it's been deprecated since Django 2.0 [1].

[1] - https://docs.djangoproject.com/en/4.1/releases/2.0/#simplified-url-routing-syntax

Signed-off-by: Isabella Basso do Amaral <idoamara@redhat.com>

osh/hub/scan/urls.py
@@ -1,20 +1,20 @@
-from django.conf.urls import url
+from django.urls import path
 
 from osh.hub.scan.views import (MockConfigListView, PackageDetailView,
                                 PackageListView)
 
 urlpatterns = [
-    url(r"^mock", MockConfigListView.as_view(),
-        name="mock_config/index"),
+    path("mock", MockConfigListView.as_view(),
+         name="mock_config/index"),
 
-    # url(r"^(?P<id>\d+)/$", "osh.hub.scan.views.scan_detail",
-    #    name="scan/detail"),
-    # url(r"^new/$", "osh.hub.scan.views.scan_submission",
-    #    name="scan/new"),
+    # path("<int:id>/", "osh.hub.scan.views.scan_detail",
+    #      name="scan/detail"),
+    # path("new/", "osh.hub.scan.views.scan_submission",
+    #      name="scan/new"),
 
-    url(r"^packages/$", PackageListView.as_view(),
-        name="package/list"),
-    url(r"^packages/(?P<pk>\d+)/detail/$",
-        PackageDetailView.as_view(),
-        name="package/detail"),
+    path("packages/", PackageListView.as_view(),
+         name="package/list"),
+    path("packages/<int:pk>/detail/",
+         PackageDetailView.as_view(),
+         name="package/detail"),
 ]

osh/hub/cs_xmlrpc/urls.py
@@ -1,15 +1,15 @@
 import kobo.django.upload.views
 import kobo.django.xmlrpc.views
-from django.conf.urls import url
+from django.urls import path
 
 urlpatterns = [
     # customize the index XML-RPC page if needed:
-    # url(r"^$", "django.views.generic.simple.direct_to_template", kwargs={"template": "xmlrpc_help.html"}, name="help/xmlrpc"),
-    url(r"^upload/", kobo.django.upload.views.file_upload),
-    url(r"^client/", kobo.django.xmlrpc.views.client_handler,
-        name="help/xmlrpc/client"),
-    url(r"^worker/", kobo.django.xmlrpc.views.worker_handler,
-        name="help/xmlrpc/worker"),
-    url(r"^kerbauth/", kobo.django.xmlrpc.views.kerbauth_handler,
-        name="help/xmlrpc/kerbauth"),
+    # path("/", "django.views.generic.simple.direct_to_template", kwargs={"template": "xmlrpc_help.html"}, name="help/xmlrpc"),
+    path("upload/", kobo.django.upload.views.file_upload),
+    path("client/", kobo.django.xmlrpc.views.client_handler,
+         name="help/xmlrpc/client"),
+    path("worker/", kobo.django.xmlrpc.views.worker_handler,
+         name="help/xmlrpc/worker"),
+    path("kerbauth/", kobo.django.xmlrpc.views.kerbauth_handler,
+         name="help/xmlrpc/kerbauth"),
 ]

osh/hub/stats/urls.py
@@ -1,26 +1,26 @@
-from django.conf.urls import url
+from django.urls import path
 
 import osh.hub.stats.views
 
 urlpatterns = [
-    url(r"^$",
-        osh.hub.stats.views.stats_list,
-        name="stats/list"),
-    url(r"^(?P<stat_id>\d+)/$",
-        osh.hub.stats.views.stats_detail,
-        name="stats/detail"),
+    path("",
+         osh.hub.stats.views.stats_list,
+         name="stats/list"),
+    path("<int:stat_id>/",
+         osh.hub.stats.views.stats_detail,
+         name="stats/detail"),
 
-    url(r"^release/(?P<release_id>\d+)/$",
-        osh.hub.stats.views.release_list,
-        name="stats/release/list"),
-    url(r"^release/(?P<release_id>\d+)/(?P<stat_id>\d+)/$",
-        osh.hub.stats.views.release_stats_detail,
-        name="stats/release/detail"),
+    path("release/<int:release_id>/",
+         osh.hub.stats.views.release_list,
+         name="stats/release/list"),
+    path("release/<int:release_id>/<int:stat_id>/",
+         osh.hub.stats.views.release_stats_detail,
+         name="stats/release/detail"),
 
-    url(r"^(?P<stat_id>\d+)/graph/$",
-        osh.hub.stats.views.stats_detail_graph,
-        name="stats/detail/graph"),
-    url(r"^(?P<stat_id>\d+)/(?P<release_id>\d+)/graph/$",
-        osh.hub.stats.views.release_stats_detail_graph,
-        name="stats/release/detail/graph"),
+    path("<int:stat_id>/graph/",
+         osh.hub.stats.views.stats_detail_graph,
+         name="stats/detail/graph"),
+    path("<int:stat_id>/<int:release_id>/graph/",
+         osh.hub.stats.views.release_stats_detail_graph,
+         name="stats/release/detail/graph"),
 ]

osh/hub/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
@@ -6,29 +6,26 @@ admin.autodiscover()
 
 
 urlpatterns = [
-    # Example:
-    # (r'^covscanhub/', include('osh.hub.foo.urls')),
-    url(r"^$", TemplateView.as_view(template_name="index.html"), name="index"),
-    url(r"^$", TemplateView.as_view(template_name="index.html"), name="home/index"),
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
+    path("", TemplateView.as_view(template_name="index.html"), name="home/index"),
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # path('admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # url(r"^$", 'osh.hub.home.views.index_redirect', name="task/list"),
-    url(r"^auth/", include("kobo.hub.urls.auth")),
-    url(r"^task/", include("osh.hub.scan.task_urls")),
-    url(r"^info/arch/", include("kobo.hub.urls.arch")),
-    url(r"^info/channel/", include("kobo.hub.urls.channel")),
-    url(r"^info/user/", include("kobo.hub.urls.user")),
-    url(r"^info/worker/", include("kobo.hub.urls.worker")),
-    url(r"^waiving/", include("osh.hub.waiving.urls")),
+    path("auth/", include("kobo.hub.urls.auth")),
+    path("task/", include("osh.hub.scan.task_urls")),
+    path("info/arch/", include("kobo.hub.urls.arch")),
+    path("info/channel/", include("kobo.hub.urls.channel")),
+    path("info/user/", include("kobo.hub.urls.user")),
+    path("info/worker/", include("kobo.hub.urls.worker")),
+    path("waiving/", include("osh.hub.waiving.urls")),
 
-    url(r"^scan/", include("osh.hub.scan.urls")),
+    path("scan/", include("osh.hub.scan.urls")),
 
-    url(r"^stats/", include("osh.hub.stats.urls")),
+    path("stats/", include("osh.hub.stats.urls")),
 
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 
     # Include kobo hub xmlrpc module urls:
-    url(r"^xmlrpc/", include("osh.hub.cs_xmlrpc.urls")),
+    path("xmlrpc/", include("osh.hub.cs_xmlrpc.urls")),
 ]

osh/hub/waiving/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 
 from osh.hub.waiving.views import (ResultsListView, et_latest,
                                    etmapping_latest, fixed_defects, new_bz,
@@ -6,38 +6,38 @@ from osh.hub.waiving.views import (ResultsListView, et_latest,
                                    remove_waiver, result, update_bz, waiver)
 
 urlpatterns = [
-    url(r"^$", ResultsListView.as_view(), name="waiving/list"),
+    path("", ResultsListView.as_view(), name="waiving/list"),
 
-    url(r"^(?P<sb_id>\d+)/(?P<result_group_id>\d+)/$",
-        waiver, name="waiving/waiver"),
-    url(r"^(?P<sb_id>\d+)/(?P<result_group_id>\d+)/fixed/$",
-        fixed_defects,
-        name="waiving/fixed_defects"),
-    url(r"^(?P<sb_id>\d+)/(?P<result_group_id>\d+)/waived/$",
-        previously_waived,
-        name="waiving/previously_waived"),
+    path("<int:sb_id>/<int:result_group_id>/",
+         waiver, name="waiving/waiver"),
+    path("<int:sb_id>/<int:result_group_id>/fixed/",
+         fixed_defects,
+         name="waiving/fixed_defects"),
+    path("<int:sb_id>/<int:result_group_id>/waived/",
+         previously_waived,
+         name="waiving/previously_waived"),
 
-    url(r"^(?P<waiver_id>\d+)/remove$",
-        remove_waiver,
-        name="waiving/waiver/remove"),
+    path("<int:waiver_id>/remove",
+         remove_waiver,
+         name="waiving/waiver/remove"),
 
-    url(r"^(?P<sb_id>\d+)/$",
-        result, name="waiving/result"),
-    url(r"^(?P<package_name>.+)/(?P<release_tag>.+)/newest/$",
-        newest_result,
-        name="waiving/result/newest"),
-    url(r"^et/(?P<et_id>.+)/$",
-        et_latest,
-        name="waiving/et_id"),
-    url(r"^et_mapping/(?P<etmapping_id>\d+)/$",
-        etmapping_latest,
-        name="waiving/etmapping_id"),
+    path("<int:sb_id>/",
+         result, name="waiving/result"),
+    path("<str:package_name>/<str:release_tag>/newest/",
+         newest_result,
+         name="waiving/result/newest"),
+    path("et/<str:et_id>/",
+         et_latest,
+         name="waiving/et_id"),
+    path("et_mapping/<int:etmapping_id>/",
+         etmapping_latest,
+         name="waiving/etmapping_id"),
 
     # BZ stuff
-    url(r"^(?P<package_id>\d+)/(?P<release_id>\d+)/newbz/$",
-        new_bz,
-        name="waiving/new_bz"),
-    url(r"^(?P<package_id>\d+)/(?P<release_id>\d+)/updatebz/$",
-        update_bz,
-        name="waiving/update_bz"),
+    path("<int:package_id>/<int:release_id>/newbz/",
+         new_bz,
+         name="waiving/new_bz"),
+    path("<int:package_id>/<int:release_id>/updatebz/",
+         update_bz,
+         name="waiving/update_bz"),
 ]

osh/hub/scan/task_urls.py
@@ -1,39 +1,59 @@
 """custom task urls"""
 
-from django.conf.urls import url
+from django.urls import path
 from kobo.client.constants import TASK_STATES
 from kobo.hub.views import TaskDetail, TaskListView, task_log, task_log_json
 
 urlpatterns = [
-    url(r"^et/$",
-        TaskListView.as_view(title="Errata Tool Tasks", ),
-        kwargs={'method': "ErrataDiffBuild"},
-        name="task/et"),
+    path("et/",
+         TaskListView.as_view(title="Errata Tool Tasks", ),
+         kwargs={'method': "ErrataDiffBuild"},
+         name="task/et"),
 
-    url(r"^$",
-        TaskListView.as_view(),
-        # kwargs={'method__in': ['DiffBuild', 'MockBuild', 'VersionDiffBuild']},
-        name="task/index"),
+    path("",
+         TaskListView.as_view(),
+         # kwargs={'method__in': ['DiffBuild', 'MockBuild', 'VersionDiffBuild']},
+         name="task/index"),
 
-    url(r"^(?P<pk>\d+)/$",
-        TaskDetail.as_view(),
-        name="task/detail"),
+    path("<int:pk>/",
+         TaskDetail.as_view(),
+         name="task/detail"),
 
-    url(r"^running/$",
-        TaskListView.as_view(title="Running Tasks", state=(TASK_STATES["FREE"], TASK_STATES["ASSIGNED"], TASK_STATES["OPEN"])),
-        name="task/running"),
+    path("running/",
+         TaskListView.as_view(
+             title="Running Tasks",
+             state=(
+                 TASK_STATES["FREE"],
+                 TASK_STATES["ASSIGNED"],
+                 TASK_STATES["OPEN"]
+             )
+         ),
+         name="task/running"),
 
-    url(r"^finished/$",
-        TaskListView.as_view(state=(TASK_STATES["CLOSED"], TASK_STATES["INTERRUPTED"], TASK_STATES["CANCELED"], TASK_STATES["FAILED"]),
-                             title="Finished Tasks",),
-        kwargs={'method__in': ['DiffBuild', 'MockBuild', 'VersionDiffBuild']},
-        name="task/finished"),
+    path("finished/",
+         TaskListView.as_view(
+             state=(
+                 TASK_STATES["CLOSED"],
+                 TASK_STATES["INTERRUPTED"],
+                 TASK_STATES["CANCELED"],
+                 TASK_STATES["FAILED"]
+             ),
+             title="Finished Tasks",
+         ),
+         kwargs={
+             'method__in': [
+                 'DiffBuild',
+                 'MockBuild',
+                 'VersionDiffBuild'
+             ]
+         },
+         name="task/finished"),
 
-    url(r"^(?P<id>\d+)/log/(?P<log_name>.+)$",
-        task_log,
-        name="task/log"),
+    path("<int:id>/log/<path:log_name>",
+         task_log,
+         name="task/log"),
 
-    url(r"^(?P<id>\d+)/log-json/(?P<log_name>.+)$",
-        task_log_json,
-        name="task/log-json"),
+    path("<int:id>/log-json/<path:log_name>",
+         task_log_json,
+         name="task/log-json"),
 ]

commit 6e79225b07452dbcbe73b807af84b7eafe462913

hub: add missing leading '/' to 'scan/mock' url
Fixes the following test failure:
```
======================================================================
FAIL: test_mockconfs_list (osh.hub.waiving.tests.BasicWebTestCase)
----------------------------------------------------------------------
Traceback (most recent call last):
  File "/usr/lib/python3.6/site-packages/django/test/utils.py", line 373, in inner
    return func(*args, **kwargs)
  File "/src/osh/hub/waiving/tests.py", line 59, in test_mockconfs_list
    self.assertEqual(r.status_code, 200)
AssertionError: 404 != 200
```

Fixes: e027357d8a2c83add2393044f4f068486d152127

osh/hub/scan/urls.py
@@ -4,7 +4,7 @@ from osh.hub.scan.views import (MockConfigListView, PackageDetailView,
                                 PackageListView)
 
 urlpatterns = [
-    path("mock", MockConfigListView.as_view(),
+    path("mock/", MockConfigListView.as_view(),
          name="mock_config/index"),
 
     # path("<int:id>/", "osh.hub.scan.views.scan_detail",