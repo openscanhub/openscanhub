openscanhub/openscanhub

commit b2abaa9a39c7e07ec001d200889a094cf73f2212

client/commands/common: remove deprecated options

osh/client/commands/cmd_diff_build.py
@@ -14,10 +14,10 @@ from osh.client.commands.analyzers import check_analyzers
 from osh.client.commands.common import (add_aggressive_option, add_all_option,
                                         add_analyzers_option,
                                         add_brew_build_option,
-                                        add_clang_option, add_comment_option,
+                                        add_comment_option,
                                         add_comp_warnings_option,
                                         add_concurrency_option,
-                                        add_config_option, add_cppcheck_option,
+                                        add_config_option,
                                         add_csmock_args_option,
                                         add_custom_model_option,
                                         add_download_results_option,
@@ -47,11 +47,9 @@ class Diff_Build(osh.client.CovScanCommand):
 ~/.config/covscan/covscan.conf"
 
         add_config_option(self.parser)
-        add_cppcheck_option(self.parser)
         add_aggressive_option(self.parser)
         add_concurrency_option(self.parser)
         add_download_results_option(self.parser)
-        add_clang_option(self.parser)
         add_comp_warnings_option(self.parser)
         add_analyzers_option(self.parser)
         add_profile_option(self.parser)

osh/client/commands/cmd_version_diff_build.py
@@ -10,10 +10,9 @@ from osh.client.commands.analyzers import check_analyzers
 from osh.common.utils.conf import get_conf
 
 from .common import (add_aggressive_option, add_all_option,
-                     add_analyzers_option, add_clang_option,
-                     add_comment_option, add_comp_warnings_option,
-                     add_concurrency_option, add_config_option,
-                     add_cppcheck_option, add_csmock_args_option,
+                     add_analyzers_option, add_comment_option,
+                     add_comp_warnings_option, add_concurrency_option,
+                     add_config_option, add_csmock_args_option,
                      add_custom_model_option, add_email_to_option,
                      add_keep_covdata_option, add_nowait_option,
                      add_priority_option, add_profile_option,
@@ -35,10 +34,8 @@ class Version_Diff_Build(osh.client.CovScanCommand):
         self.parser.epilog = "User configuration file is located at: \
 ~/.config/covscan/covscan.conf"
 
-        add_cppcheck_option(self.parser)
         add_aggressive_option(self.parser)
         add_concurrency_option(self.parser)
-        add_clang_option(self.parser)
         add_comp_warnings_option(self.parser)
         add_analyzers_option(self.parser)
         add_profile_option(self.parser)

osh/client/commands/common.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-def add_cppcheck_option(parser):
-    parser.add_option(
-        "-c",
-        "--cppcheck",
-        default=False,
-        action="store_true",
-        help="enable Cppcheck analyzer (DEPRECATED in favour of -a cppcheck)",
-    )
-
-
 def add_aggressive_option(parser):
     parser.add_option(
         "--aggressive",
@@ -37,16 +27,6 @@ def add_download_results_option(parser):
         dest="results_dir",
         help="directory for storing results (leave blank for working \
 directory)"
-    )
-
-
-def add_clang_option(parser):
-    parser.add_option(
-        "-l",
-        "--clang",
-        default=False,
-        action="store_true",
-        help="enable Clang analyzer (DEPRECATED in favour of -a clang)"
     )
 
 

commit e0c698765a28e939d17ae810fd8049ef9a6d4d6f

client: remove handling of removed options
Fixes: b2abaa9a39c7e07ec001d200889a094cf73f2212 ("client/commands/common: remove deprecated options")

osh/client/commands/cmd_diff_build.py
@@ -68,7 +68,6 @@ exist." % self.results_store_file)
 
         # optparser output is passed via *args (args) and **kwargs (opts)
         config = kwargs.pop("config", None)
-        cppcheck = kwargs.pop("cppcheck", None)
         email_to = kwargs.pop("email_to", [])
         comment = kwargs.pop("comment")
         nowait = kwargs.pop("nowait")
@@ -76,7 +75,6 @@ exist." % self.results_store_file)
         priority = kwargs.pop("priority")
         brew_build = kwargs.pop("brew_build")
         self.results_store_file = kwargs.pop("results_dir", None)
-        clang = kwargs.pop('clang', False)
         warn_level = kwargs.pop('warn_level', '0')
         analyzers = kwargs.pop('analyzers', '')
         profile = kwargs.pop('profile', None)
@@ -129,10 +127,6 @@ is not even one in your user configuration file \
         if priority is not None:
             options["priority"] = priority
 
-        if cppcheck:
-            options["cppcheck"] = cppcheck
-        if clang:
-            options['clang'] = clang
         if warn_level:
             options['warning_level'] = warn_level
         if analyzers:

osh/client/commands/cmd_version_diff_build.py
@@ -81,7 +81,6 @@ local file"
 
         config = kwargs.pop("config", None)
         base_config = kwargs.pop("base_config", None)
-        cppcheck = kwargs.pop("cppcheck", None)
         email_to = kwargs.pop("email_to", [])
         comment = kwargs.pop("comment")
         nowait = kwargs.pop("nowait")
@@ -91,7 +90,6 @@ local file"
         brew_build = kwargs.pop("brew_build", None)
         base_srpm = kwargs.pop("base_srpm", None)
         srpm = kwargs.pop("srpm", None)
-        clang = kwargs.pop('clang', False)
         warn_level = kwargs.pop('warn_level', '0')
         analyzers = kwargs.pop('analyzers', '')
         profile = kwargs.pop('profile', None)
@@ -189,10 +187,6 @@ is not even one in your user configuration file \
         if priority is not None:
             options_consumed["priority"] = priority
 
-        if cppcheck:
-            options_forwarded["cppcheck"] = cppcheck
-        if clang:
-            options_forwarded['clang'] = clang
         if warn_level:
             options_forwarded['warning_level'] = warn_level
         if analyzers: