openscanhub/openscanhub

commit bcc5b4d5431863eabbb0f8bda8a651d6e60c02a9

hub: remove dead code in the errata module
... and also the corresponding dead code from other modules.

Fixes: a446007ddd3caf5f7531139be79d4f9a85730541
Fixes: 3f1a10eed612dd4bba328f7e12084be38c3c2e4e
Fixes: 1edd956e4f774cc3ba99b048d9f93c3c4a3406d9
Resolves: https://gitlab.cee.redhat.com/covscan/covscan/-/issues/193

osh/hub/scan/service.py
@@ -3,7 +3,6 @@
 """
 
 
-import copy
 import logging
 import os
 import shlex
@@ -19,12 +18,11 @@ from osh.common.constants import (ERROR_DIFF_FILE, ERROR_HTML_FILE,
                                   ERROR_TXT_FILE, FIXED_DIFF_FILE,
                                   FIXED_HTML_FILE, FIXED_TXT_FILE)
 from osh.hub.other.exceptions import ScanException
-from osh.hub.other.shortcuts import (check_and_create_dirs, check_brew_build,
-                                     get_mock_by_name)
+from osh.hub.other.shortcuts import check_and_create_dirs, check_brew_build
 from osh.hub.service.processing import add_title_to_json
 
 from .models import (SCAN_STATES, SCAN_STATES_FINISHED_WELL, SCAN_TYPES_TARGET,
-                     Analyzer, Scan, ScanBinding)
+                     Scan, ScanBinding)
 
 logger = logging.getLogger(__name__)
 
@@ -251,105 +249,6 @@ def create_base_diff_task(hub_opts, task_opts, parent_id):
         # move file to task dir, remove upload record and make the task available
         shutil.move(srpm_path, os.path.join(task_dir, os.path.basename(srpm_path)))
         upload.delete()
-
-
-def create_diff_task(hub_opts, task_opts):
-    """
-        create scan of a package and perform diff on results against specified
-        version
-        Options for task are stored in 'task_opts'
-        options of this scan are in dict 'hub_opts':
-
-        hub_opts
-         - task_user - username from request.user.username
-         - nvr_srpm - name, version, release of scanned package
-         - nvr_upload_id - upload id for target, so worker is able to download it
-         - nvr_brew_build - NVR of package to be downloaded from brew
-         - base_srpm - name, version, release of base package
-         - base_upload_id - upload id for base, so worker is able to download it
-         - base_brew_build - NVR of base package to be downloaded from brew
-         - nvr_mock - mock config
-         - base_mock - mock config
-    """
-    options = task_opts or {}
-
-    task_user = hub_opts['task_user']
-
-    nvr_srpm = hub_opts.get('nvr_srpm', None)
-    nvr_brew_build = hub_opts.get('nvr_brew_build', None)
-    base_brew_build = hub_opts.get('base_brew_build', None)
-    nvr_upload_id = hub_opts.get('nvr_upload_id', None)
-    analyzers = hub_opts.pop("analyzers", None)
-
-    # Label, description or any reason for this task.
-    task_label = nvr_srpm or nvr_brew_build
-
-    nvr_mock = hub_opts['nvr_mock']
-    base_mock = hub_opts['base_mock']
-    priority = hub_opts.get('priority', 10)
-    comment = hub_opts.get('comment', '')
-
-    # does mock config exist?
-    get_mock_by_name(nvr_mock)
-    options["mock_config"] = nvr_mock
-    # if base config is invalid target task isn't submited, is this alright?
-    get_mock_by_name(base_mock)
-
-    # Test if SRPM exists
-    if base_brew_build:
-        check_brew_build(base_brew_build)
-    if nvr_brew_build:
-        options['brew_build'] = check_brew_build(nvr_brew_build)
-    elif nvr_upload_id:
-        try:
-            upload = FileUpload.objects.get(id=nvr_upload_id)
-        except FileUpload.DoesNotExist:
-            raise ObjectDoesNotExist("Can't find uploaded file with id: %s" % nvr_upload_id)
-
-        if upload.owner.username != task_user:
-            raise RuntimeError("Can't process a file uploaded by a different user")
-
-        srpm_path = os.path.join(upload.target_dir, upload.name)
-        options["srpm_name"] = upload.name
-        # cut .src.rpm suffix, because run_diff and extractTarball rely on this
-        task_label = options["srpm_name"][:-8]
-    else:
-        raise RuntimeError('Target build is not specified!')
-
-    if analyzers:
-        an_conf = Analyzer.objects.get_opts(analyzers)
-        options.update(an_conf)
-        task_opts.update(an_conf)
-
-    task_id = Task.create_task(
-        owner_name=task_user,
-        label=task_label,
-        method='VersionDiffBuild',
-        args=options,
-        comment=comment,
-        state=TASK_STATES["FREE"],
-        priority=priority
-    )
-    task_dir = Task.get_task_dir(task_id)
-
-    check_and_create_dirs(task_dir)
-
-    if nvr_upload_id:
-        # move file to task dir, remove upload record and make the task
-        # available
-        shutil.move(srpm_path, os.path.join(task_dir,
-                                            os.path.basename(srpm_path)))
-        upload.delete()
-
-    parent_task = Task.objects.get(id=task_id)
-    create_base_diff_task(copy.deepcopy(hub_opts), task_opts, task_id)
-
-    # wait has to be after creation of new subtask
-    # TODO wait should be executed in one transaction with creation of
-    # child
-    parent_task.wait()
-
-    return task_id
 
 
 def get_latest_sb_by_package(release, package):

osh/hub/errata/service.py
@@ -1,127 +1,14 @@
 import logging
-import re
 
-from django.conf import settings
 from kobo.hub.models import TASK_STATES, Task
 
-from osh.hub.errata.check import check_nvr, check_package_eligibility
-from osh.hub.errata.utils import _spawn_scan_task, spawn_scan_task
 from osh.hub.other.exceptions import ScanException
-from osh.hub.other.shortcuts import check_and_create_dirs, check_brew_build
-from osh.hub.scan.models import (SCAN_STATES, SCAN_STATES_IN_PROGRESS,
-                                 SCAN_TYPES, SCAN_TYPES_TARGET, AppSettings,
-                                 ETMapping, MockConfig, Package,
-                                 ReleaseMapping, Scan, ScanBinding)
-from osh.hub.scan.service import get_latest_binding, get_latest_sb_by_package
-from osh.hub.scan.xmlrpc_helper import cancel_scan
-from osh.hub.service.processing import task_has_results
+from osh.hub.other.shortcuts import check_and_create_dirs
+from osh.hub.scan.models import (SCAN_STATES, ETMapping, ReleaseMapping,
+                                 ScanBinding)
+from osh.hub.scan.service import get_latest_binding
 
 logger = logging.getLogger(__name__)
-
-
-def create_errata_base_scan(d, parent_task_id):
-    """Create base scan according to dict kwargs"""
-    options = {}
-
-    d['scan_type'] = SCAN_TYPES['ERRATA_BASE']
-    # kwargs['target'] = TARGET, d['target'] = BASE
-    d['target'] = d['base']
-    del d['base']
-    # we don't want bases tagged
-    d['tag'] = None
-
-    d['task_label'] = d['target']
-    options['brew_build'] = d['target']
-
-    d.setdefault('priority', settings.ET_SCAN_PRIORITY + 1)
-    d['comment'] = 'Errata Tool Base scan of %s requested by %s' % \
-        (d['target'], d['target'])
-
-    # Test if SRPM exists
-    check_brew_build(d['target'])
-
-    # set profile to be same as target's
-    options['mock_config'] = d['mock_config']
-
-    d['method'] = 'ErrataDiffBuild'
-    d['parent_id'] = parent_task_id
-    d['scan_enabled'] = False
-    task_id, scan = _spawn_scan_task(d)
-
-    options["scan_id"] = scan.id
-
-    # DO NOT USE filter...update() -- invalid json is filled in db
-    task = Task.objects.get(id=task_id)
-    task.args = options
-    task.save()
-    task.free_task()
-
-    sb = ScanBinding()
-    sb.task = task
-    sb.scan = scan
-    sb.save()
-
-    return scan
-
-
-def obtain_base(d, task_id):
-    """
-    @type task_id - int (parent task ID)
-    @type d - dict (dict with scan settings)
-    """
-    binding = get_latest_binding(d['base'])
-    found = bool(binding)
-    if found:
-        actual_scanner = AppSettings.settings_actual_scanner()
-        if ((binding.scan.state == SCAN_STATES['QUEUED']
-            or binding.scan.state == SCAN_STATES['SCANNING'])
-                and binding.result is None):
-            return binding.scan
-        elif binding.result is None:
-            found = False
-        elif binding.result.scanner_version != actual_scanner[1] or \
-                binding.result.scanner != actual_scanner[0]:
-            found = False
-        elif not task_has_results(binding.task):
-            found = False
-    if not found:
-        parent_task = Task.objects.get(id=task_id)
-        base_obj = create_errata_base_scan(d, task_id)
-
-        # wait has to be after creation of new subtask
-        # TODO wait should be executed in one transaction with creation of
-        # child
-        parent_task.wait()
-        return base_obj
-    return binding.scan
-
-
-def check_obsolete_scan(package, release):
-    bindings = ScanBinding.objects.filter(
-        scan__package=package,
-        scan__tag__release=release,
-        scan__scan_type__in=SCAN_TYPES_TARGET)
-    for binding in bindings:
-        if binding.scan.state in SCAN_STATES_IN_PROGRESS:
-            cancel_scan(binding.scan.id)
-
-
-def assign_mock_config(dist_tag):
-    """
-        NOT USED:, base scan inherits mock profile from target
-
-        Assign appropriate mock config according to 'dist_tag', if this fails
-        fallback to rhel-6 -- there is at least some output
-    """
-    try:
-        release = re.match(r".+\.el(\d)", dist_tag).group(1)
-        mock = MockConfig.objects.get(name="rhel-%s-x86_64" % release)
-    except Exception as ex:  # noqa: B902
-        logger.error("Unable to find proper mock profile for dist_tag %s: %s"
-                     % (dist_tag, ex))
-        return MockConfig.objects.get(get="rhel-6-x86_64").name
-    else:
-        return mock.name
 
 
 def get_tag(release):
@@ -141,109 +28,6 @@ def return_or_raise(key, data):
         logger.error("Key '%s' is missing from dict '%s'", key, data)
         raise RuntimeError("Key '%s' is missing from %s, invalid scan \
 submission!" % (key, data))
-
-
-def create_errata_scan(kwargs, etm):
-    """
-    create scan of a package and perform diff on results against specified
-     version
-    options of this scan are in dict 'kwargs'
-
-    kwargs
-     - package_owner - name of the package for the advisory owner
-     - target - name, version, release of scanned package (brew build)
-     - base - previous version of package, the one to make diff against
-     - id - ET internal id for the scan record in ET
-     - errata_id - the ET internal id of the advisory that the build is part of
-     - rhel_version - short tag of rhel version -- product (e. g. 'RHEL-6.3.Z')
-     - release - The advisory's release ('ASYNC', 'RHEL-.*', 'MRG.*')
-
-    return ETMapping
-    """
-    # dict stored in Task.args and used by task in worker
-    options = {}
-
-    # dict for creation of Scan and Task objects
-    d = kwargs.copy()
-
-    # be sure that dict contains all necessary data
-    return_or_raise('package_owner', kwargs)
-    return_or_raise('target', kwargs)
-    return_or_raise('base', kwargs)
-
-    # validation of nvr
-    target_nvre_dict = check_nvr(kwargs['target'])
-
-    # first thing, create entry in DB about provided package
-    package, created = Package.objects.get_or_create(
-        name=target_nvre_dict['name'])
-
-    # The advisory's release (mainly for knowledge of advisory being 'ASYNC')
-    # values: RHEL-6.2.0, RHEL-6.2.z, ASYNC
-    release = return_or_raise('release', kwargs)
-    # returns (mock config's name, tag object)
-    tag = get_tag(release)
-    if tag:
-        options['mock_config'] = tag.mock.name
-        d['mock_config'] = tag.mock.name
-    else:
-        raise RuntimeError("Unable to assign mock profile.")
-    check_obsolete_scan(package, tag.release)
-    d['tag'] = tag
-
-    # check if package is written in scannable language and is not blocked
-    check_package_eligibility(package, kwargs['target'],
-                              options['mock_config'], tag.release,
-                              created)
-    d['package'] = package
-
-    # one of RHEL-6.2.0, RHEL-6.2.z, etc.
-    # rhel_version = return_or_raise('rhel_version', kwargs)
-
-    options['brew_build'] = kwargs['target']
-
-    # Label, description or any reason for this task.
-    d['task_label'] = kwargs['target']
-
-    d.setdefault('priority', settings.ET_SCAN_PRIORITY)
-    d['comment'] = 'Errata Tool Scan of %s' % kwargs['target']
-
-    # Test if build exists
-    # TODO: add check if SRPM exist:
-    #    GET /brewroot/.../package/version-release/...src.rpm
-    check_brew_build(kwargs['target'])
-
-    child = get_latest_sb_by_package(d['tag'].release, d['package'])
-
-    # is it rebase? new pkg? or ordinary scan? spawn appropriate models to DB
-    task_id, scan = spawn_scan_task(d, target_nvre_dict)
-
-    if scan.can_have_base():
-        base = obtain_base(d.copy(), task_id)
-        scan.base = base
-        scan.save()
-
-    if child and child.scan:
-        child_scan = Scan.objects.get(id=child.scan.id)
-        child_scan.parent = scan
-        child_scan.enabled = False
-        child_scan.save()
-
-    options['scan_id'] = scan.id
-    task = Task.objects.get(id=task_id)
-    task.args = options
-    task.save()
-    task.free_task()
-
-    sb = ScanBinding()
-    sb.task = task
-    sb.scan = scan
-    sb.save()
-
-    etm.latest_run = sb
-    etm.save()
-
-    scan.set_state(SCAN_STATES['QUEUED'])
 
 
 def rescan(scan, user):

osh/hub/errata/check.py
@@ -26,17 +26,6 @@ def check_nvr(nvr):
         raise RuntimeError('%s is not a correct N-V-R' % nvr)
 
 
-def check_package_eligibility(package, nvr, mock_profile, release, created):
-    if created:
-        logger.info('Package %s for %s was created', package, release)
-        # all freshly scanned packages are now eligible by default
-    else:
-        is_blocked = package.is_blocked(release)
-        if is_blocked:
-            raise PackageBlockedException('Package %s is blocked.' %
-                                          (package.name))
-
-
 def check_package_is_blocked(package, release):
     is_blocked = package.is_blocked(release)
     if is_blocked:

osh/hub/errata/utils.py
@@ -1,27 +1,8 @@
 import logging
 
-import koji
-from django.conf import settings
-from kobo.hub.models import TASK_STATES, Task
 from kobo.rpmlib import parse_nvr
 
-from osh.hub.other.shortcuts import check_and_create_dirs
-from osh.hub.scan.models import SCAN_TYPES, Scan
-
-__all__ = (
-    "spawn_scan_task",
-    "_spawn_scan_task",
-)
-
-if __name__ == '__main__':
-    logger = logging.getLogger('osh.hub.errata.utils')
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    logger.addHandler(ch)
-else:
-    logger = logging.getLogger(__name__)
-
-s = koji.ClientSession(settings.BREW_URL)
+logger = logging.getLogger(__name__)
 
 
 def get_or_fail(key, data):
@@ -33,80 +14,8 @@ def get_or_fail(key, data):
         raise RuntimeError("Key '%s' is missing from '%s'!" % (key, data))
 
 
-def _spawn_scan_task(d):
-    """
-    parent method that actually creates Task and Scan
-
-    @type d: dict
-    """
-    task_id = Task.create_task(
-        owner_name=d['task_user'],
-        label=d['task_label'],
-        method=d['method'],
-        args={},  # I want to add scan's id here, so I update it later
-        comment=d['comment'],
-        state=TASK_STATES["CREATED"],
-        priority=d['priority'],
-        parent_id=d.get('parent_id', None),
-    )
-    task_dir = Task.get_task_dir(task_id)
-
-    check_and_create_dirs(task_dir)
-
-    scan = Scan.create_scan(scan_type=d['scan_type'], nvr=d['target'],
-                            username=d['package_owner'],
-                            tag=d['tag'], package=d['package'],
-                            enabled=d['scan_enabled'])
-    return task_id, scan
-
-##########
-# SPAWNING
-##########
-
-
-def spawn_newpkg(d):
-    d.update((
-        ('method', 'ErrataDiffBuild'),
-        ('scan_type', SCAN_TYPES['NEWPKG']),
-    ), )
-    return _spawn_scan_task(d)
-
-
-def spawn_rebase(d):
-    d.update((
-        ('method', 'ErrataDiffBuild'),
-        ('scan_type', SCAN_TYPES['REBASE']),
-    ), )
-    return _spawn_scan_task(d)
-
-
-def spawn_classic(d):
-    d.update((
-        ('method', 'ErrataDiffBuild'),
-        ('scan_type', SCAN_TYPES['ERRATA']),
-    ), )
-    return _spawn_scan_task(d)
-
-
 def is_rebase(base, target):
     """ base, target -- NVRs """
     base_d = parse_nvr(base)
     target_d = parse_nvr(target)
     return target_d['version'] != base_d['version']
-
-
-def spawn_scan_task(d, target):
-    """
-    Figure out scan type and create scan and task models
-    Exported method
-
-    @type d: dict
-    @type target: dict (save one parse_nvr call)
-    """
-    d['scan_enabled'] = True
-    if d['base'].lower() == 'new_package':
-        return spawn_newpkg(d)
-    elif is_rebase(d['base'], target):
-        return spawn_rebase(d)
-    else:
-        return spawn_classic(d)

osh/hub/other/shortcuts.py
@@ -2,13 +2,11 @@ import os
 
 import koji
 from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
 from django.urls import reverse
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
 from osh.hub.other.exceptions import BrewException
-from osh.hub.scan.models import MockConfig
 
 
 def add_link_field(target_model=None, field='', field_label=''):
@@ -34,16 +32,6 @@ def add_link_field(target_model=None, field='', field_label=''):
             [field_name]
         return cls
     return add_link
-
-
-def get_mock_by_name(name):
-    try:
-        conf = MockConfig.objects.get(name=name)
-    except:  # noqa: B902, E722
-        raise ObjectDoesNotExist("Unknown mock config: %s" % name)
-    if not conf.enabled:
-        raise RuntimeError("Mock config is disabled: %s" % conf)
-    return conf
 
 
 def check_brew_build(name):

commit 347e558606518ce9ba852f3bbfaef015ea0d902e

hub: remove create_base_diff_task function
... because it is unused and it was not removed in previous clean-ups
by a mistake.

Fixes: bcc5b4d5431863eabbb0f8bda8a651d6e60c02a9 ("hub: remove dead code in the errata module")

osh/hub/scan/service.py
@@ -6,11 +6,8 @@
 import logging
 import os
 import shlex
-import shutil
 
 from django.core.exceptions import ObjectDoesNotExist
-from kobo.client.constants import TASK_STATES
-from kobo.django.upload.models import FileUpload
 from kobo.hub.models import Task
 from kobo.shortcuts import run
 
@@ -18,7 +15,6 @@ from osh.common.constants import (ERROR_DIFF_FILE, ERROR_HTML_FILE,
                                   ERROR_TXT_FILE, FIXED_DIFF_FILE,
                                   FIXED_HTML_FILE, FIXED_TXT_FILE)
 from osh.hub.other.exceptions import ScanException
-from osh.hub.other.shortcuts import check_and_create_dirs, check_brew_build
 from osh.hub.service.processing import add_title_to_json
 
 from .models import (SCAN_STATES, SCAN_STATES_FINISHED_WELL, SCAN_TYPES_TARGET,
@@ -176,79 +172,6 @@ def extract_logs_from_tarball(task_id, name=None):
     except RuntimeError:
         raise RuntimeError('[%s] Unable to extract tarball archive %s \
 I have used this command: %s' % (task_id, tar_archive, command))
-
-
-def create_base_diff_task(hub_opts, task_opts, parent_id):
-    """
-        create scan of a package and perform diff on results against specified
-        version
-        options of this scan are in dict 'kwargs'
-
-        kwargs
-         - task_user - username from request.user.username
-         - nvr_srpm - name, version, release of scanned package
-         - nvr_upload_id - upload id for target, so worker is able to download it
-         - nvr_brew_build - NVR of package to be downloaded from brew
-         - base_srpm - name, version, release of base package
-         - base_upload_id - upload id for base, so worker is able to download it
-         - base_brew_build - NVR of base package to be downloaded from brew
-         - nvr_mock - mock config
-         - base_mock - mock config
-    """
-    options = task_opts or {}
-
-    base_srpm = hub_opts.get('base_srpm', None)
-    base_brew_build = hub_opts.get('base_brew_build', None)
-    base_upload_id = hub_opts.get('base_upload_id', None)
-
-    # from request.user
-    task_user = hub_opts['task_user']
-
-    # Label, description or any reason for this task.
-    task_label = base_srpm or base_brew_build
-
-    base_mock = hub_opts['base_mock']
-    priority = hub_opts.get('priority', 10) + 1
-    comment = hub_opts.get('comment', '')
-
-    options["mock_config"] = base_mock
-
-    if base_brew_build:
-        options['brew_build'] = check_brew_build(base_brew_build)
-    elif base_upload_id:
-        try:
-            upload = FileUpload.objects.get(id=base_upload_id)
-        except FileUpload.DoesNotExist:
-            raise ObjectDoesNotExist("Can't find uploaded file with id: %s" % base_upload_id)
-
-        if upload.owner.username != task_user:
-            raise RuntimeError("Can't process a file uploaded by a different user")
-
-        srpm_path = os.path.join(upload.target_dir, upload.name)
-        options["srpm_name"] = upload.name
-        # cut .src.rpm suffix, because run_diff and extractTarball rely on this
-        task_label = options["srpm_name"][:-8]
-    else:
-        raise RuntimeError('Target build is not specified!')
-
-    task_id = Task.create_task(
-        owner_name=task_user,
-        label=task_label,
-        method='VersionDiffBuild',
-        args=options,
-        comment=comment,
-        state=TASK_STATES["FREE"],
-        priority=priority,
-        parent_id=parent_id,
-    )
-    task_dir = Task.get_task_dir(task_id)
-
-    check_and_create_dirs(task_dir)
-
-    if base_upload_id:
-        # move file to task dir, remove upload record and make the task available
-        shutil.move(srpm_path, os.path.join(task_dir, os.path.basename(srpm_path)))
-        upload.delete()
 
 
 def get_latest_sb_by_package(release, package):