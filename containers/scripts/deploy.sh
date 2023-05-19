openscanhub/openscanhub

commit 9bd249885992cd64cd4757ad7ff98479e1af65ad

containers/scripts/utils: provide `wait_for_db()` helper
... and use it in `init-db.sh` and `build.sh` to eliminate duplicated code.

This also makes the `build.sh` script wait for containers to start
before releasing control.

Signed-off-by: Isabella Basso do Amaral <idoamara@redhat.com>

containers/scripts/utils.sh
@@ -18,7 +18,23 @@ export_host_variables() {
 
 export_host_variables
 
-#Checks osh container status
+# Checks db container status
+#
+# Returns:
+# 0 if db is ready, 1 if it isn't started in 60s
+wait_for_db() {
+  count=0
+  while ! podman exec -it osh-hub pg_isready -h db; do
+    sleep 1
+    count=$((count + 1))
+    if test "$count" -gt 60; then
+      return 1
+    fi
+  done
+  return 0
+}
+
+# Checks osh container status
 #
 # @param $1 container name (e.g. hub, worker, client)
 #

.gitlab-ci.yml
@@ -36,7 +36,6 @@ django-unit-tests:
       - htmlcov/
   before_script:
     - git clone --depth 1 https://github.com/release-engineering/kobo.git
-    - while ! podman exec -it osh-hub pg_isready -h db; do sleep 0.5; done
     - podman exec -it db psql -c 'ALTER USER covscanhub CREATEDB;'
   script:
     - podman exec -it osh-hub /usr/bin/coverage-3.6 run --omit="*site-packages*,*kobo*," covscanhub/manage.py test -v 3 || exit 1

containers/scripts/build.sh
@@ -24,6 +24,11 @@ main() {
   eval podman-compose "$PROFILE" down
 
   eval podman-compose up --build "$START" "$CONTAINERS"
+
+  if [ "$START" = '-d' ]; then
+    wait_for_container 'HUB'
+    wait_for_db
+  fi
 }
 
 #Used for testing the build environment

containers/scripts/init-db.sh
@@ -3,19 +3,15 @@
 # shellcheck disable=1091
 source containers/scripts/utils.sh
 
-set -e
-set -x
+main() {
+  set -ex
+  # weak password used for testing purposes only
+  PASSWD=xxxxxx
 
-# weak password used for testing purposes only
-PASSWD=xxxxxx
-
-# start db and osh-hub to apply migrations
-./containers/scripts/build.sh --run
-
-(set +e; wait_for_container 'HUB')
-
-# create covscan users
-podman exec -i osh-hub python3 covscanhub/manage.py shell << EOF
+  # start db and osh-hub to apply migrations
+  ./containers/scripts/build.sh --run
+  # create covscan users
+  podman exec -i osh-hub python3 covscanhub/manage.py shell << EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
 print(User.objects.create_superuser('admin', 'kdudka@redhat.com', '${PASSWD}'))
@@ -24,9 +20,12 @@ for login in ['idoamara', 'kdudka', 'lbalhar']:
     print(User.objects.create_user(login, 'f{login}@redhat.com', '${PASSWD}'))
 EOF
 
-# dump the database to a file
-podman exec -i db pg_dump -h localhost -U covscanhub \
+  # dump the database to a file
+  podman exec -i db pg_dump -h localhost -U covscanhub \
     | gzip -c > covscanhub-minimal.db.gz
 
-# print summary on success
-file covscanhub-minimal.db.gz
+  # print summary on success
+  file covscanhub-minimal.db.gz
+}
+
+main

commit 2ceaf1be508683718a9ca92cd90f372a782b0afa

containers: scripts: drop wait_for_db function
As the hub already waits for the db to start, we do not have to wait for
it explicitly. This function is also removed as it's used nowhere.

Fixes: 9bd2498 ("containers/scripts/utils: provide `wait_for_db()` helper")

Signed-off-by: Isabella do Amaral <idoamara@redhat.com>

containers/scripts/deploy.sh
@@ -41,7 +41,6 @@ main() {
 
     if [ "$START" = '-d' ]; then
         wait_for_container 'HUB'
-        wait_for_db
     fi
 }
 

containers/scripts/utils.sh
@@ -18,22 +18,6 @@ export_host_variables() {
 
 export_host_variables
 
-# Checks db container status
-#
-# Returns:
-# 0 if db is ready, 1 if it isn't started in 60s
-wait_for_db() {
-    count=0
-    while ! podman exec -it osh-hub pg_isready -h db; do
-        sleep 1
-        count=$((count + 1))
-        if test "$count" -gt 60; then
-            return 1
-        fi
-    done
-    return 0
-}
-
 # Checks osh container status
 #
 # @param $1 container name (e.g. hub, worker, client)