openscanhub/openscanhub

commit 0e9102ba5670168f12364be53edd5d978d2bc38f

hub: Rename `covscanhub` to `openscanhub` in logger
Related: https://gitlab.cee.redhat.com/covscan/covscan/-/issues/154

osh/hub/settings_local.py
@@ -57,7 +57,7 @@ LOGGING = {
         }
     },
     'loggers': {
-        'covscanhub': {
+        'opencanhub': {
             'handlers': ['file', 'console'],
             'level': 'DEBUG',
             'propagate': True,

osh/hub/cs_xmlrpc/errata.py
@@ -14,7 +14,7 @@ __all__ = (
     "get_scan_state",
 )
 
-logger = logging.getLogger("covscanhub")
+logger = logging.getLogger("openscanhub")
 
 
 @login_required

osh/hub/cs_xmlrpc/scan.py
@@ -13,7 +13,7 @@ from osh.hub.errata.scanner import (ClientDiffPatchesScanScheduler,
                                     ClientScanScheduler, create_diff_task2)
 from osh.hub.scan.models import SCAN_STATES, ClientAnalyzer, Profile, Scan
 
-logger = logging.getLogger("covscanhub")
+logger = logging.getLogger("openscanhub")
 
 __all__ = (
     "diff_build",

osh/hub/settings_local.prod.py
@@ -50,7 +50,7 @@ LOGGING = {
         },
     },
     'loggers': {
-        'covscanhub': {
+        'opencanhub': {
             'handlers': ['file'],
             'level': 'DEBUG',
             'propagate': True,

osh/hub/settings_local.stage.py
@@ -49,7 +49,7 @@ LOGGING = {
         },
     },
     'loggers': {
-        'covscanhub': {
+        'openscanhub': {
             'handlers': ['file'],
             'level': 'DEBUG',
             'propagate': True,

commit 6eeab4381f940d3ff72a91d94b6f82b12e257d53

hub: use correct logger prefix in settings_local*.py
Fixes: 0e9102ba5670168f12364be53edd5d978d2bc38f ("hub: Rename `covscanhub` to `openscanhub` in logger")

osh/hub/settings_local.ci.py
@@ -51,7 +51,7 @@ LOGGING = {
         },
     },
     'loggers': {
-        'openscanhub': {
+        'osh': {
             'handlers': ['file'],
             'level': 'DEBUG',
             'propagate': True,

osh/hub/settings_local.py
@@ -58,7 +58,7 @@ LOGGING = {
         }
     },
     'loggers': {
-        'opencanhub': {
+        'osh': {
             'handlers': ['file', 'console'],
             'level': 'DEBUG',
             'propagate': True,