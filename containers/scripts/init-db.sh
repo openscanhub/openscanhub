openscanhub/openscanhub

commit dc9a8e2da421eb460edd40b55350bcf111c6d294

containers/scripts: merge database restore into init-db script
Signed-off-by: Isabella Basso do Amaral <idoamara@redhat.com>

containers/scripts/init-db.sh
@@ -3,13 +3,27 @@
 # shellcheck disable=1091
 source containers/scripts/utils.sh
 
+DEPLOY='false'
+MINIMAL='true'
+
 main() {
   set -ex
+
+  if [ "$DEPLOY" = true ]; then
+    ./containers/scripts/deploy.sh
+  fi
+
+  if [ "$MINIMAL" = true ]; then
+    minimal
+  else
+    restore
+  fi
+}
+
+minimal() {
   # weak password used for testing purposes only
   PASSWD=xxxxxx
 
-  # start db and osh-hub to apply migrations
-  ./containers/scripts/build.sh --run
   # create covscan users
   podman exec -i osh-hub python3 covscanhub/manage.py shell << EOF
 from django.contrib.auth import get_user_model
@@ -27,5 +41,61 @@ EOF
   # print summary on success
   file covscanhub-minimal.db.gz
 }
+
+restore() {
+  FILEPATH='https://covscan-stage.lab.eng.brq2.redhat.com/covscanhub.db.gz'
+  DOWNLOAD=true
+
+  if [ -e covscanhub.db.gz ]; then
+    if [ "$(echo "$(curl "${FILEPATH}.SHA512SUM" | cut -d'=' -f2)" covscanhub.db.gz | sha512sum --check)" ]; then
+      DOWNLOAD=false
+    fi
+  fi
+
+  if [ "$DOWNLOAD" = true ]; then
+    DOWNLOAD_COMMAND='curl -O'
+    [ "$({ which aria2c 2>&1; } > /dev/null)" ] && DOWNLOAD_COMMAND='aria2c -s10'
+    (set -x; eval "$DOWNLOAD_COMMAND $FILEPATH")
+  fi
+
+  (\
+    set -x
+    gzip -cd covscanhub.db.gz | podman exec -i db psql -h localhost -U osh-hub
+    # HACK: this should be turned into a function
+    # ref: https://stackoverflow.com/a/16853755/9814181
+    podman exec -i osh-hub python3 covscanhub/manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+User.objects.create_user('user', 'user@redhat.com', 'xxxxxx')
+u = User.objects.get(username='admin')
+u.set_password('xxxxxx')
+u.save()
+EOF
+  )
+}
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --deploy)
+      DEPLOY='true'
+      shift
+      ;;
+    --minimal)
+      shift
+      ;;
+    --restore)
+      MINIMAL='false'
+      shift
+      ;;
+    *)
+      if [ -z "$1" ]; then
+        shift
+      else
+        echo "Unknown option: $1"
+        exit 22 # EINVAL
+      fi
+      ;;
+  esac
+done
 
 main

.gitlab-ci.yml
@@ -22,7 +22,7 @@ run-hub-and-minimal-db:
     - gitlab-job-guard --private-token="$GITLAB_API_TOKEN"
     - git clone --depth 1 https://github.com/release-engineering/kobo.git
   script:
-    - containers/scripts/init-db.sh
+    - containers/scripts/init-db.sh --deploy --minimal
 
 django-unit-tests:
   needs:

containers/scripts/deploy.sh
@@ -4,58 +4,17 @@
 source containers/scripts/utils.sh
 
 DEBUG=false
-INTERACTIVE=''
 
 main() {
-  local FILEPATH
-  local DOWNLOAD
-
   test_deploy_env || exit "$?"
 
+  ./containers/scripts/build.sh --run "$FULL_DEV"
+
   if [[ "$DEBUG" = true ]]; then
-    ./containers/scripts/init-db.sh || exit "$?"
-
-    (set -x; podman start osh-worker)
-
-    if [[ "$(type podman)" =~ docker || "$FULL_DEV" = true ]]; then
-      (set -x; podman run -it "$INTERACTIVE" osh-client bash)
-    fi
-
-    return 0
+    ./containers/scripts/init-db.sh --minimal || exit "$?"
+  else
+    ./containers/scripts/init-db.sh --restore || exit "$?"
   fi
-
-  ./containers/scripts/build.sh --run
-
-  FILEPATH='https://covscan-stage.lab.eng.brq2.redhat.com/covscanhub.db.gz'
-  DOWNLOAD=true
-
-  [ -e covscanhub.db.gz ] &&\
-    [ "$(echo "$(curl "${FILEPATH}.SHA512SUM" | cut -d'=' -f2)" covscanhub.db.gz | sha512sum --check)" ] &&\
-    DOWNLOAD=false
-
-  if [ "$DOWNLOAD" = true ]; then
-    DOWNLOAD_COMMAND='curl -O'
-    [ "$({ which aria2c 2>&1; } > /dev/null)" ] && DOWNLOAD_COMMAND='aria2c -s10'
-    (set -x; eval "$DOWNLOAD_COMMAND $FILEPATH")
-  fi
-
-  (\
-    set -x
-    gzip -cd covscanhub.db.gz | podman exec -i db psql -h localhost -U osh-hub
-    # HACK: this should be turned into a function
-    # ref: https://stackoverflow.com/a/16853755/9814181
-    podman exec -i osh-hub python3 covscanhub/manage.py shell << EOF
-from django.contrib.auth import get_user_model
-User = get_user_model()
-User.objects.create_user('user', 'user@redhat.com', 'xxxxxx')
-u = User.objects.get(username='admin')
-u.set_password('xxxxxx')
-u.save()
-EOF
-  )
-  # TODO: insert worker config
-
-  (set -x; podman start osh-worker)
 }
 
 #Used for testing the deployment environment
@@ -77,11 +36,7 @@ while [[ $# -gt 0 ]]; do
       shift
       ;;
     --full-dev)
-      FULL_DEV=true
-      shift
-      ;;
-    --no-interactive)
-      INTERACTIVE='-d'
+      FULL_DEV='--full-dev'
       shift
       ;;
     *)

commit eecc62b2263c85323c1883f01709f44bca553810

containers: scripts/init-db: fix db restore
We need to start the hub so it configures the db for us. But we can't
(and shouldn't) use Django's `loaddata` because of the format of our
data, and because it is slower than restoring directly, thus we need to
stop the server. We also need to reset the db while keeping permissions,
so we drop it and recreate it with the default user (set in
compose.yaml).

Notice that if we hadn't started the hub we also wouldn't be able to
access the db without overriding the defaults.

Fixes: dc9a8e2d ("containers/scripts: merge database restore into init-db script")

Signed-off-by: Isabella do Amaral <idoamara@redhat.com>

containers/scripts/init-db.sh
@@ -16,7 +16,7 @@ help() {
     echo "  -f, --force     Force compose down"
     echo "  -F, --full-dev  Create a system-independent development environment"
     echo "  -m, --minimal   Create a minimal database"
-    echo "  -r, --restore   Auto-restore database from backup"
+    echo "  --restore       Auto-restore database from backup (DANGEROUS)"
 }
 
 main() {
@@ -27,6 +27,10 @@ main() {
     if [ "$MINIMAL" = true ]; then
         minimal
     elif [ "$RESTORE" = true ]; then
+        if [ "$FORCE" = false ]; then
+            echo "--restore must be used with --force"
+            exit 1
+        fi
         restore
     else
         help
@@ -63,7 +67,12 @@ restore() {
 
     curl -O "https://covscan-stage.lab.eng.brq2.redhat.com/${FILENAME}"
 
+    podman stop osh-hub
+    podman exec db dropdb openscanhub
+    podman exec db createdb openscanhub
     gzip -cd openscanhub.db.gz | podman exec -i db psql -h localhost -U openscanhub
+    podman start osh-hub
+    wait_for_container 'HUB'
     # HACK: this should be turned into a function
     # ref: https://stackoverflow.com/a/16853755/9814181
     podman exec -i osh-hub python3 osh/hub/manage.py shell << EOF