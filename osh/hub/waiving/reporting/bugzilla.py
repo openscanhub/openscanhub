2d2b8a1fcd23ef2a30fb10ffc996bb8758bf8ccb
Isabella do Amaral <idoamara@redhat.com>
2023-06-30 14:13:50 -0300

    hub: waiving/reporting: standardize interface with classes
    Signed-off-by: Isabella do Amaral <idoamara@redhat.com>

--- osh/hub/waiving/reporting/bugzilla.py
+++ osh/hub/waiving/reporting/bugzilla.py
@@ -12,88 +12,91 @@ from osh.hub.other import get_or_none
 from osh.hub.waiving.models import WAIVER_TYPES, Bugzilla, ResultGroup, Waiver
 
 
-def get_client():
-    xmlrpc_url = urljoin(settings.BZ_URL, 'xmlrpc.cgi')
-    return bugzilla.Bugzilla(url=xmlrpc_url, api_key=settings.BZ_API_KEY)
+class BugzillaReporter:
+    def __init__(self, package, release):
+        self.package = package
+        self.release = release
 
+    def has_bug(self):
+        """
+        return True if there is BZ created for specified package/release
+        """
+        return get_or_none(Bugzilla, package=self.package, release=self.release)
 
-def has_bug(package, release):
-    """
-    return True if there is BZ created for specified package/release
-    """
-    return get_or_none(Bugzilla, package=package, release=release)
-
-
-def get_unreported_bugs(package, release):
-    """
-    return IS_A_BUG waivers that weren't reported yet
-    """
-    rgs = ResultGroup.objects.select_related().filter(
-        result__scanbinding__scan__package=package,
-        result__scanbinding__scan__tag__release=release,
-    )
-    waivers = Waiver.waivers.filter(
-        result_group__result__scanbinding__scan__package=package,
-        result_group__result__scanbinding__scan__tag__release=release,
-        state__in=[WAIVER_TYPES['IS_A_BUG'], WAIVER_TYPES['FIX_LATER']],
-        bz__isnull=True,
-        id__in=[rg.has_waiver().id for rg in rgs if rg.has_waiver()]
-    )
-    if waivers:
-        return waivers.order_by('date')
-
-
-def format_waivers(waivers, request):
-    """
-    return output of waivers/defects that is posted to bugzilla
-    """
-    comment = ''
-    for w in waivers:
-        comment += "Group: %s\nDate: %s\nMessage: %s\nLink: %s\n" % (
-            w.result_group.checker_group.name,
-            w.date.strftime('%Y-%m-%d %H:%M:%S %Z'),  # 2013-01-04 04:09:51 EST
-            w.message,
-            request.build_absolute_uri(
-                reverse('waiving/waiver',
-                        args=(w.result_group.result.scanbinding.id,
-                              w.result_group.id))
-            )
+    def get_unreported_bugs(self):
+        """
+        return IS_A_BUG waivers that weren't reported yet
+        """
+        rgs = ResultGroup.objects.select_related().filter(
+            result__scanbinding__scan__package=self.package,
+            result__scanbinding__scan__tag__release=self.release,
         )
-        if w != waivers.reverse()[0]:
-            comment += "-" * 78 + "\n\n"
-    return comment
+        waivers = Waiver.waivers.filter(
+            result_group__result__scanbinding__scan__package=self.package,
+            result_group__result__scanbinding__scan__tag__release=self.release,
+            state__in=[WAIVER_TYPES['IS_A_BUG'], WAIVER_TYPES['FIX_LATER']],
+            bz__isnull=True,
+            id__in=[rg.has_waiver().id for rg in rgs if rg.has_waiver()]
+        )
+        if waivers:
+            return waivers.order_by('date')
 
+    @staticmethod
+    def format_waivers(waivers, request):
+        """
+        return output of waivers/defects that is posted to bugzilla
+        """
+        comment = ''
+        for w in waivers:
+            comment += "Group: %s\nDate: %s\nMessage: %s\nLink: %s\n" % (
+                w.result_group.checker_group.name,
+                w.date.strftime('%Y-%m-%d %H:%M:%S %Z'),  # 2013-01-04 04:09:51 EST
+                w.message,
+                request.build_absolute_uri(
+                    reverse('waiving/waiver',
+                            args=(w.result_group.result.scanbinding.id,
+                                  w.result_group.id))
+                )
+            )
+            if w != waivers.reverse()[0]:
+                comment += "-" * 78 + "\n\n"
+        return comment
 
-def get_checker_groups(waivers):
-    s = ""
-    for n in waivers.values('result_group__checker_group__name').distinct():
-        s += "%s\n" % n['result_group__checker_group__name']
-    return s
+    @staticmethod
+    def get_checker_groups(waivers):
+        s = ""
+        for n in waivers.values('result_group__checker_group__name').distinct():
+            s += "%s\n" % n['result_group__checker_group__name']
+        return s
 
+    @staticmethod
+    def __get_client():
+        xmlrpc_url = urljoin(settings.BZ_URL, 'xmlrpc.cgi')
+        return bugzilla.Bugzilla(url=xmlrpc_url, api_key=settings.BZ_API_KEY)
 
-def create_bug(request, package, release):
-    """
-    create bugzilla for package/release and fill it with all IS_A_BUG waivers
-    this function should be called by view -- button "Create Bugzilla"
-    """
-    bz = get_client()
-    waivers = get_unreported_bugs(package, release)
+    def create_bug(self, request):
+        """
+        create bugzilla for package/release and fill it with all IS_A_BUG waivers
+        this function should be called by view -- button "Create Bugzilla"
+        """
+        bz = self.__get_client()
+        waivers = self.get_unreported_bugs()
 
-    if not waivers:
-        raise ValueError("No waivers to report")
+        if not waivers:
+            raise ValueError("No waivers to report")
 
-    scan = waivers[0].result_group.result.scanbinding.scan
+        scan = waivers[0].result_group.result.scanbinding.scan
 
-    if scan.base:
-        base = scan.base.nvr
-    else:
-        base = "NEW_PACKAGE"
+        if scan.base:
+            base = scan.base.nvr
+        else:
+            base = "NEW_PACKAGE"
 
-    target = scan.nvr
-    groups = get_checker_groups(waivers)
+        target = scan.nvr
+        groups = self.get_checker_groups(waivers)
 
-    comment = f"""
-Csmock has found defect(s) in package {package.name}s
+        comment = f"""
+Csmock has found defect(s) in package {self.package.name}s
 
 Package was scanned as differential scan:
 
@@ -105,64 +108,64 @@ Package was scanned as differential scan:
 == Marked waivers ==
 """
 
-    summary = 'New defect%s found in %s' % (
-        's' if waivers.count() >= 2 else '',
-        scan.nvr)
+        summary = 'New defect%s found in %s' % (
+            's' if waivers.count() >= 2 else '',
+            scan.nvr)
 
-    comment += format_waivers(waivers, request)
+        comment += self.format_waivers(waivers, request)
 
-    data = {
-        'product': release.product,
-        'component': package.name,
-        'summary': summary,  # 'short_desc'
-        'version': release.version,
-        'comment': comment,
-        'bug_severity': 'medium',
-        'priority': 'high',
-        'rep_platform': 'All',
-        'op_sys': 'Linux',
-        'groups': ['private'],  # others are devel, qa
-        'cc': [],
-    }
+        data = {
+            'product': self.release.product,
+            'component': self.package.name,
+            'summary': summary,  # 'short_desc'
+            'version': self.release.version,
+            'comment': comment,
+            'bug_severity': 'medium',
+            'priority': 'high',
+            'rep_platform': 'All',
+            'op_sys': 'Linux',
+            'groups': ['private'],  # others are devel, qa
+            'cc': [],
+        }
 
-    if scan.username != request.user:
-        data['cc'].append(f'{request.user.username}@redhat.com')
+        if scan.username != request.user:
+            data['cc'].append(f'{request.user.username}@redhat.com')
 
-    try:
-        b = bz.createbug(**data)
-    except Fault:
-        # most likely the email in CC does not exist in BZ as user
-        del data['cc']
-        b = bz.createbug(**data)
-    db_bz = Bugzilla()
-    db_bz.release = release
-    db_bz.package = package
-    db_bz.number = b.bug_id
-    db_bz.save()
-    for w in waivers:
-        w.bz = db_bz
-        w.save()
+        try:
+            b = bz.createbug(**data)
+        except Fault:
+            # most likely the email in CC does not exist in BZ as user
+            del data['cc']
+            b = bz.createbug(**data)
 
+        db_bz = Bugzilla()
+        db_bz.release = self.release
+        db_bz.package = self.package
+        db_bz.number = b.bug_id
+        db_bz.save()
+        for w in waivers:
+            w.bz = db_bz
+            w.save()
 
-def update_bug(request, package, release):
-    """
-    add defects to specified bugzilla that aren't there yet
-    this function should be called by view -- button "update bugzilla"
-    """
-    bz = get_client()
-    db_bz = has_bug(package, release)
-    if not db_bz:
-        create_bug(request, package, release)
-        return
+    def update_bug(self, request):
+        """
+        add defects to specified bugzilla that aren't there yet
+        this function should be called by view -- button "update bugzilla"
+        """
+        bz = self.__get_client()
+        db_bz = self.has_bug()
+        if not db_bz:
+            self.create_bug(request)
+            return
 
-    waivers = get_unreported_bugs(package, release)
+        waivers = self.get_unreported_bugs()
 
-    if not waivers:
-        raise ValueError("No waivers to report")
+        if not waivers:
+            raise ValueError("No waivers to report")
 
-    comment = format_waivers(waivers, request)
-    bzbug = bz.getbug(db_bz.number)
-    bzbug.addcomment(comment, False)
-    for w in waivers:
-        w.bz = db_bz
-        w.save()
+        comment = self.format_waivers(waivers, request)
+        bzbug = bz.getbug(db_bz.number)
+        bzbug.addcomment(comment, False)
+        for w in waivers:
+            w.bz = db_bz
+            w.save()
--- osh/hub/waiving/reporting/jira.py
+++ osh/hub/waiving/reporting/jira.py
@@ -9,96 +9,99 @@ from osh.hub.other import get_or_none
 from osh.hub.waiving.models import WAIVER_TYPES, JiraBug, ResultGroup, Waiver
 
 
-def has_bug(package, release):
-    """
-    returns True if there is a jira issue created for specified package/release
-    """
-    return get_or_none(JiraBug, package=package, release=release)
+class JiraReporter:
+    def __init__(self, package, release):
+        self.package = package
+        self.release = release
 
+    def has_bug(self):
+        """
+        returns True if there is a jira issue created for specified package/release
+        """
+        return get_or_none(JiraBug, package=self.package, release=self.release)
 
-def get_unreported_bugs(package, release):
-    """
-    returns IS_A_BUG waivers that weren't reported yet
-    """
-    rgs = ResultGroup.objects.select_related().filter(
-        result__scanbinding__scan__package=package,
-        result__scanbinding__scan__tag__release=release,
-    )
-    waivers = Waiver.waivers.filter(
-        result_group__result__scanbinding__scan__package=package,
-        result_group__result__scanbinding__scan__tag__release=release,
-        state__in=[WAIVER_TYPES['IS_A_BUG'], WAIVER_TYPES['FIX_LATER']],
-        jira_bug__isnull=True,
-        id__in=[rg.has_waiver().id for rg in rgs if rg.has_waiver()]
-    )
-    if waivers:
-        return waivers.order_by('date')
-
-
-def format_waivers(waivers, request):
-    """
-    returns output of waivers/defects that is posted to jira
-    """
-    comment = ''
-    for w in waivers:
-        comment += "Group: %s\nDate: %s\nMessage: %s\nLink: %s\n" % (
-            w.result_group.checker_group.name,
-            w.date.strftime('%Y-%m-%d %H:%M:%S %Z'),  # 2013-01-04 04:09:51 EST
-            w.message,
-            request.build_absolute_uri(
-                reverse('waiving/waiver',
-                        args=(w.result_group.result.scanbinding.id,
-                              w.result_group.id))
-            )
+    def get_unreported_bugs(self):
+        """
+        returns IS_A_BUG waivers that weren't reported yet
+        """
+        rgs = ResultGroup.objects.select_related().filter(
+            result__scanbinding__scan__package=self.package,
+            result__scanbinding__scan__tag__release=self.release,
         )
-        if w != waivers.reverse()[0]:
-            comment += "-" * 78 + "\n\n"
-    return comment
+        waivers = Waiver.waivers.filter(
+            result_group__result__scanbinding__scan__package=self.package,
+            result_group__result__scanbinding__scan__tag__release=self.release,
+            state__in=[WAIVER_TYPES['IS_A_BUG'], WAIVER_TYPES['FIX_LATER']],
+            jira_bug__isnull=True,
+            id__in=[rg.has_waiver().id for rg in rgs if rg.has_waiver()]
+        )
+        if waivers:
+            return waivers.order_by('date')
 
+    @staticmethod
+    def format_waivers(waivers, request):
+        """
+        returns output of waivers/defects that is posted to jira
+        """
+        comment = ''
+        for w in waivers:
+            comment += "Group: %s\nDate: %s\nMessage: %s\nLink: %s\n" % (
+                w.result_group.checker_group.name,
+                w.date.strftime('%Y-%m-%d %H:%M:%S %Z'),  # 2013-01-04 04:09:51 EST
+                w.message,
+                request.build_absolute_uri(
+                    reverse('waiving/waiver',
+                            args=(w.result_group.result.scanbinding.id,
+                                  w.result_group.id))
+                )
+            )
+            if w != waivers.reverse()[0]:
+                comment += "-" * 78 + "\n\n"
+        return comment
 
-def get_checker_groups(waivers):
-    s = ""
-    for n in waivers.values('result_group__checker_group__name').distinct():
-        s += "%s\n" % n['result_group__checker_group__name']
-    return s
+    @staticmethod
+    def get_checker_groups(waivers):
+        s = ""
+        for n in waivers.values('result_group__checker_group__name').distinct():
+            s += "%s\n" % n['result_group__checker_group__name']
+        return s
 
-
-def get_client():
-    options = {
-        'server': settings.JIRA_URL,
-        'token_auth': settings.JIRA_API_KEY
-    }
-    if 'stage' in settings.JIRA_URL:
-        options['proxies'] = {
-            'http': 'http://squid.corp.redhat.com:3128',
-            'https': 'http://squid.corp.redhat.com:3128'
+    @staticmethod
+    def __get_client():
+        options = {
+            'server': settings.JIRA_URL,
+            'token_auth': settings.JIRA_API_KEY
         }
-    return JIRA(**options)
+        if 'stage' in settings.JIRA_URL:
+            options['proxies'] = {
+                'http': 'http://squid.corp.redhat.com:3128',
+                'https': 'http://squid.corp.redhat.com:3128'
+            }
+        return JIRA(**options)
 
+    def create_bug(self, request):
+        """
+        create jira issue for package/release and fill it with all IS_A_BUG waivers
+        this function should be called by view -- button "Create Jira Issue"
+        """
+        jira = self.__get_client()
+        waivers = self.get_unreported_bugs()
 
-def create_bug(request, package, release):
-    """
-    create jira issue for package/release and fill it with all IS_A_BUG waivers
-    this function should be called by view -- button "Create Jira Issue"
-    """
-    jira = get_client()
-    waivers = get_unreported_bugs(package, release)
+        if not waivers:
+            raise ValueError("No waivers to report")
 
-    if not waivers:
-        raise ValueError("No waivers to report")
+        scan = waivers[0].result_group.result.scanbinding.scan
 
-    scan = waivers[0].result_group.result.scanbinding.scan
+        if scan.base:
+            base = scan.base.nvr
+        else:
+            base = "NEW_PACKAGE"
 
-    if scan.base:
-        base = scan.base.nvr
-    else:
-        base = "NEW_PACKAGE"
+        target = scan.nvr
+        groups = self.get_checker_groups(waivers)
 
-    target = scan.nvr
-    groups = get_checker_groups(waivers)
-
-    comment = f"""
-Csmock has found defect(s) in package {package.name}s
+        comment = f"""
+Csmock has found defect(s) in package {self.package.name}s
 
 Package was scanned as differential scan:
 
@@ -110,75 +113,74 @@ Package was scanned as differential scan:
 == Marked waivers ==
 """
 
-    summary = 'New defect%s found in %s' % (
-        's' if waivers.count() >= 2 else '',
-        scan.nvr)
+        summary = 'New defect%s found in %s' % (
+            's' if waivers.count() >= 2 else '',
+            scan.nvr)
 
-    comment += format_waivers(waivers, request)
+        comment += self.format_waivers(waivers, request)
 
-    # rhel version should be in format 'rhel-X.y.0{.z}'
-    version = f'rhel-{release.version}.0'
-    if release.is_parent():
-        version += '.z'
+        # rhel version should be in format 'rhel-X.y.0{.z}'
+        version = f'rhel-{self.release.version}.0'
+        if self.release.is_parent():
+            version += '.z'
 
-    data = {
-        'project': {
-            'key': 'RHEL',
-        },
-        'summary': summary,
-        'components': [
-            {
-                'name': package.name,
+        data = {
+            'project': {
+                'key': 'RHEL',
             },
-        ],
-        'versions': [
-            {
-                'name': version,
+            'summary': summary,
+            'components': [
+                {
+                    'name': self.package.name,
+                },
+            ],
+            'versions': [
+                {
+                    'name': version,
+                },
+            ],
+            # temporarily using custom field because issues.redhat.com doesn't have
+            # the 'priority' field
+            # see: https://gitlab.cee.redhat.com/covscan/covscan/-/issues/218
+            'customfield_12316142': {  # Severity
+                'id': '15655',  # Normal
             },
-        ],
-        # temporarily using custom field because issues.redhat.com doesn't have
-        # the 'priority' field
-        # see: https://gitlab.cee.redhat.com/covscan/covscan/-/issues/218
-        'customfield_12316142': {  # Severity
-            'id': '15655',  # Normal
-        },
-        'issuetype': 'Bug',
-        'security': {
-            'name': 'Red Hat Employee',
-        },
-    }
+            'issuetype': 'Bug',
+            'security': {
+                'name': 'Red Hat Employee',
+            },
+        }
 
-    issue_key = jira.create_issue(fields=data).key
-    jira.add_comment(issue_key, comment)
+        issue_key = jira.create_issue(fields=data).key
+        jira.add_comment(issue_key, comment)
 
-    db_jira = JiraBug()
-    db_jira.release = release
-    db_jira.package = package
-    db_jira.key = issue_key
-    db_jira.save()
-    for w in waivers:
-        w.jira_bug = db_jira
-        w.save()
+        db_jira = JiraBug()
+        db_jira.release = self.release
+        db_jira.package = self.package
+        db_jira.key = issue_key
+        db_jira.save()
+        for w in waivers:
+            w.jira_bug = db_jira
+            w.save()
 
+    def update_bug(self, request):
+        """
+        add defects to specified bugzilla that aren't there yet
+        this function should be called by view -- button "update bugzilla"
+        """
+        jira = self.__get_client()
+        db_jira = self.has_bug()
+        if not db_jira:
+            self.create_bug(request)
+            return
 
-def update_bug(request, package, release):
-    """
-    add defects to specified bugzilla that aren't there yet
-    this function should be called by view -- button "update bugzilla"
-    """
-    jira = get_client()
-    db_jira = has_bug(package, release)
-    if not db_jira:
-        create_bug(request, package, release)
-        return
+        waivers = self.get_unreported_bugs()
 
-    waivers = get_unreported_bugs(package, release)
+        if not waivers:
+            raise ValueError("No waivers to report")
 
-    if not waivers:
-        raise ValueError("No waivers to report")
-
-    comment = format_waivers(waivers, request)
-    jira.add_comment(db_jira.key, comment)
-    for w in waivers:
-        w.jira_bug = db_jira
-        w.save()
+        comment = self.format_waivers(waivers, request)
+        jira.add_comment(db_jira.key, comment)
+        for w in waivers:
+            w.jira_bug = db_jira
+            w.save()
