openscanhub/openscanhub

commit 4af45c74b02335ed0b15e7d4185b66a16210caed

containers: scripts/deploy: simplify prepare_deploy
There was needless complexity in this function.

Fixes: 795319c7 ("containers/scripts/deploy: fix --force behavior")

Signed-off-by: Isabella do Amaral <idoamara@redhat.com>

containers/scripts/deploy.sh
@@ -27,7 +27,7 @@ help() {
     echo
     echo "Options:"
     echo "  --clean          Remove all containers and volumes"
-    echo "  -f, --force      Force container rebuild"
+    echo "  -f, --force      Force compose down"
     echo "  -F, --full-dev   Create a system-independent development environment"
     echo "  -h, --help       Show this message"
     echo "  --no-start       Do not start containers"
@@ -38,9 +38,7 @@ main() {
 
     test_build_env || exit "$?"
 
-    if [ "$START" = '-d' ]; then
-        prepare_deploy
-    fi
+    prepare_deploy
 
     podman-compose -p osh up --build $START "${CONTAINERS[@]}"
 
@@ -79,36 +77,24 @@ prepare_deploy() {
     test_deploy_env || exit "$?"
 
     if [ "$IS_LINUX" = 1 ]; then
-        LABEL_PREFIX='io.podman'
+        LABEL='io.podman'
     else
-        LABEL_PREFIX='com.docker'
+        LABEL='com.docker'
     fi
+    LABEL+='.compose.project=osh'
 
-    containers_count="$(podman ps -a --filter label="$LABEL_PREFIX".compose.project=osh --filter status=running -q 2>/dev/null | wc -l)"
+    mapfile -t running < <(podman ps $LABEL -q 2>/dev/null)
 
-    if [[ "$containers_count" -gt 0 ]]; then
-        if [ "$FORCE" = true ]; then
-            # when running the down command docker won't stop osh-client if not
-            # specified causing errors when trying to remove the network
-            # we also can't specify container names in down command
-            containers=""
-            if [ "$IS_LINUX" = 1 ]; then
-                containers="db osh-hub osh-worker"
-                if [ "$containers_count" -gt 4 ]; then
-                    containers+=" osh-client"
-                fi
-            fi
-            podman-compose -p osh $PROFILE down $containers
-            return
-        else
-            # shellcheck disable=2016
-            echo 'One or more containers are already running under `compose`. Please use `compose down` to kill them.'
-            exit 1
-        fi
+    if [[ ${#running[@]} -gt 0 ]] && [ "$FORCE" = false ]; then
         # shellcheck disable=2016
         echo 'One or more containers are already running under `compose`. Please use `compose down` to kill them.'
         exit 4
     fi
+
+    if [ "$IS_LINUX" != 1 ]; then
+        running=()
+    fi
+    podman-compose -p osh $PROFILE down "${running[@]}"
 }
 
 

containers/scripts/generate_integration_test_coverage.sh
@@ -140,7 +140,7 @@ while [[ $# -gt 0 ]]; do
             echo "Usage: $0 [--force|-f]"
             echo
             echo "Options:"
-            echo "  -f, --force  Force container rebuild"
+            echo "  -f, --force  Force compose down"
             exit 0
             ;;
         --force)

containers/scripts/init-db.sh
@@ -13,7 +13,7 @@ help() {
     echo
     echo "Options:"
     echo "  -h, --help      Show this message"
-    echo "  -f, --force     Force container rebuild"
+    echo "  -f, --force     Force compose down"
     echo "  -F, --full-dev  Create a system-independent development environment"
     echo "  -m, --minimal   Create a minimal database"
     echo "  -r, --restore   Auto-restore database from backup"

commit 31337d2eb70c7a4845a45d7a1630114257037fdd

containers: fix compose cleanup when --force is used
Fixes the following traceback:
```
+ podman-compose -p osh down a69f95600469 91ed93afa2b3 5e4b6337c75b
podman-compose version: 1.0.6
['podman', '--version', '']
using podman version: 4.5.1
Traceback (most recent call last):
  File "/usr/bin/podman-compose", line 8, in <module>
    sys.exit(main())
             ^^^^^^
  File "/usr/lib/python3.11/site-packages/podman_compose.py", line 2941, in main
    podman_compose.run()
  File "/usr/lib/python3.11/site-packages/podman_compose.py", line 1423, in run
    cmd(self, args)
  File "/usr/lib/python3.11/site-packages/podman_compose.py", line 1754, in wrapped
    return func(*args, **kw)
           ^^^^^^^^^^^^^^^^^
  File "/usr/lib/python3.11/site-packages/podman_compose.py", line 2149, in compose_down
    excluded = get_excluded(compose, args)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "/usr/lib/python3.11/site-packages/podman_compose.py", line 2020, in get_excluded
    excluded -= compose.services[service]["_deps"]
                ~~~~~~~~~~~~~~~~^^^^^^^^^
KeyError: 'a69f95600469'
```

Fixes: 4af45c74b02335ed0b15e7d4185b66a16210caed ("containers: scripts/deploy: simplify prepare_deploy")

containers/scripts/deploy.sh
@@ -31,7 +31,7 @@ help() {
     echo
     echo "Options:"
     echo "  --clean          Remove all containers and volumes"
-    echo "  -f, --force      Force compose down"
+    echo "  -f, --force      Force container rebuild"
     echo "  -F, --full-dev   Create a system-independent development environment"
     echo "  -h, --help       Show this message"
     echo "  --no-start       Do not start containers"
@@ -76,14 +76,11 @@ prepare_deploy() {
 
     if [[ ${#running[@]} -gt 0 ]] && [ "$FORCE" = false ]; then
         # shellcheck disable=2016
-        echo 'One or more containers are already running under `compose`. Please use `compose down` to kill them.'
+        echo 'One or more containers are already running under `compose`. Please use `--clean` to kill them.'
         return 4
     fi
 
-    if [ "$IS_LINUX" != 1 ]; then
-        running=()
-    fi
-    podman-compose -p osh $PROFILE down "${running[@]}"
+    clean
 }
 
 