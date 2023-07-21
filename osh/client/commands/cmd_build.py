3024022354f20b5641fc1e4ee537b74eaead85d5
Lukáš Zaoral <lzaoral@redhat.com>
2023-07-21 14:59:07 +0200

    client: factor out checking of builds and mock configs

--- osh/client/commands/cmd_build.py
+++ osh/client/commands/cmd_build.py
@@ -1,6 +1,8 @@
 import os
 from xmlrpc.client import Fault
 
+from kobo.shortcuts import random_string
+
 from osh.client import OshCommand
 from osh.client.commands.common import (add_analyzers_option,
                                         add_comment_option,
@@ -15,7 +17,10 @@ from osh.client.commands.common import (add_analyzers_option,
                                         add_priority_option,
                                         add_profile_option,
                                         add_task_id_file_option)
-from osh.client.commands.shortcuts import fetch_results, handle_perm_denied
+from osh.client.commands.shortcuts import (fetch_results, handle_perm_denied,
+                                           upload_file, verify_koji_build,
+                                           verify_mock)
+from osh.client.conf import get_conf
 
 
 class Base_Build(OshCommand):
@@ -41,6 +46,49 @@ class Base_Build(OshCommand):
         add_priority_option(self.parser)
         add_profile_option(self.parser)
         add_task_id_file_option(self.parser)
+
+    def check_build(self, args, kwargs):
+        local_conf = get_conf(self.conf)
+
+        config = kwargs.get("config")
+        nvr = kwargs.get("nvr")
+        srpm = kwargs.get("srpm")
+        tarball_build_script = kwargs.get("tarball_build_script")
+
+        if not config:
+            config = local_conf.get_default_mockconfig()
+            if not config:
+                self.parser.error("You haven't specified mock config, there \
+is not even one in your user configuration file \
+(~/.config/osh/client.conf) nor in system configuration file \
+(/etc/osh/client.conf)")
+            print("config not specified, using default one:", config)
+
+        result = verify_mock(config, self.hub)
+        if result is not None:
+            self.parser.error(result)
+
+        options = {"mock_config": config}
+
+        if nvr:
+            # get build from koji
+            koji_profiles = self.conf.get('KOJI_PROFILES', 'brew,koji')
+            result = verify_koji_build(nvr, koji_profiles)
+            if result is not None:
+                self.parser.error(result)
+            options["brew_build"] = nvr
+        else:
+            # we are analyzing tarball with build script
+            if tarball_build_script:
+                if not os.path.exists(srpm):
+                    self.parser.error("Tarball does not exist.")
+                options['tarball_build_script'] = tarball_build_script
+
+            target_dir = random_string(32)
+            upload_id, *_ = upload_file(self.hub, srpm, target_dir, self.parser)
+            options["upload_id"] = upload_id
+
+        return options
 
     def prepare_task_options(self, args, kwargs):
         pass
