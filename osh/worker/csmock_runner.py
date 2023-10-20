openscanhub/openscanhub

commit 8bfae2dfa6410105afdfd603d0f9cb939f6ffa21

worker: use the `-t` option for cspodman, too
cspodman recently introduced the `-t` option compatible with csmock.
So we can propagate the list of enabled analyzers for both the tools.

Related: https://issues.redhat.com/browse/OSH-313
Closes: https://github.com/openscanhub/openscanhub/pull/141

osh/worker/csmock_runner.py
@@ -123,10 +123,11 @@ class CsmockRunner:
             cmd += " --force-global-cleanup-on-exit"
         else:
             cmd = "csmock"
-            if analyzers:
-                cmd += ' -t ' + shlex.quote(analyzers)
             if profile:
                 cmd += ' -r ' + shlex.quote(profile)
+
+        if analyzers:
+            cmd += ' -t ' + shlex.quote(analyzers)
 
         if output_path:
             cmd += ' -o ' + shlex.quote(output_path)

commit 8fbb2246bc29fece989eeb2d8d70b17aad6131bb

worker: use the `-t` option for `cspodman --no-scan`, too
As pointed out by Lukáš Zaoral, the change merged as
https://github.com/openscanhub/openscanhub/pull/141 was incomplete.
There is another copy of the code in `no_scan()`, which needs to be
updated, too.

Fixes: commit 8bfae2dfa6410105afdfd603d0f9cb939f6ffa21
Related: https://issues.redhat.com/browse/OSH-313
Closes: https://github.com/openscanhub/openscanhub/pull/164

osh/worker/csmock_runner.py
@@ -210,10 +210,10 @@ class CsmockRunner:
             cmd = "cspodman"
         else:
             cmd = "csmock"
-            cmd += ' -t ' + shlex.quote(analyzers)
             if profile:
                 cmd += ' -r ' + shlex.quote(profile)
 
+        cmd += ' -t ' + shlex.quote(analyzers)
         cmd += ' --no-scan'
         if additional_arguments:
             cmd += ' ' + additional_arguments