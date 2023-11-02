openscanhub/openscanhub

commit 0c8b9ce6345f3221099fb9955a94236fc1c3dda8

Remove get_files function from include.py
We can implement the same logic in a simpler way
directly in setup.py.

Fixes: https://gitlab.cee.redhat.com/covscan/covscan/-/issues/155

setup.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import os
+from glob import glob
+from pathlib import Path
 
 from setuptools import PEP420PackageFinder, setup
 
-from scripts.include import (get_files, get_git_date_and_time, get_git_version,
+from scripts.include import (get_git_date_and_time, get_git_version,
                              git_check_tag_for_HEAD)
 
 find_namespace_packages = PEP420PackageFinder.find
@@ -44,6 +46,7 @@ package_data = {
     ]
 }
 
+hub_path = Path("covscanhub")
 for folder in (
     "static",
     "templates",
@@ -52,7 +55,10 @@ for folder in (
     "errata/fixtures",
     "fixtures",
 ):
-    package_data["covscanhub"].extend(get_files("covscanhub", folder))
+    for path in glob(str(hub_path / folder / "**"), recursive=True):
+        path = Path(path)
+        if path.is_file():
+            package_data["covscanhub"].append(str(path.relative_to(hub_path)))
 
 if os.path.isdir(".git"):
     if not git_check_tag_for_HEAD(THIS_FILE_PATH):

scripts/include.py
@@ -1,21 +1,6 @@
 # -*- coding: utf-8 -*-
 
-
-import os
 import subprocess
-
-
-def get_files(module_name, top_dir):
-    """Return list of all files under top_dir."""
-    result = []
-
-    module = __import__(module_name)
-    module_dir = os.path.dirname(module.__file__)
-
-    for root, dirs, files in os.walk(os.path.join(module_dir, top_dir)):
-        for fn in files:
-            result.append(os.path.join(top_dir, root, fn)[len(module_dir) + 1:])
-    return result
 
 
 def get_git_date_and_time(git_repo_path):

commit 441c9aa75050a8ed003104639258e7258691f3b9

packaging: remove redundant glob import from setup.py
Fixes: 0c8b9ce6345f3221099fb9955a94236fc1c3dda8 ("Remove get_files function from include.py")

setup.py
@@ -2,7 +2,6 @@
 # SPDX-FileCopyrightText: Copyright contributors to the OpenScanHub project.
 
 import os
-from glob import glob
 from pathlib import Path
 
 from setuptools import PEP420PackageFinder, setup
@@ -54,8 +53,7 @@ for folder in (
     "errata/fixtures",
 ):
 
-    for path in glob(str(hub_path / folder / "**"), recursive=True):
-        path = Path(path)
+    for path in (hub_path / folder).rglob("*"):
         if path.is_file():
             package_data["osh"].append(str(path.relative_to("osh")))
 