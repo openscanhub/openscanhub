openscanhub/openscanhub

commit 460aeea953a1918e55ad2ce4891c573be322297e

hub: make ErrataDiffBuild task independent of ScanningSession
... to make the cache_task_args and get_scanning_args more generic.

osh/hub/errata/scanner.py
@@ -70,7 +70,9 @@ class AbstractScheduler:
         """ prepare dicts -- arguments for task and scan """
         self.task_args['args'] = {}
         self.task_args['args']['build'] = self.nvr
-        self.task_args['args']['scanning_session'] = self.scanning_session.id
+        self.task_args['args']['profile'] = 'errata'
+        # TODO: remove the 'scanning_session' argument when workers are updated
+        self.task_args['args']['scanning_session'] = None
         self.task_args['args']['su_user'] = AppSettings.setting_get_su_user()
         self.scan_args['nvr'] = self.nvr
         self.scan_args['username'] = self.package_owner

osh/hub/osh_xmlrpc/worker.py
@@ -15,7 +15,7 @@ from osh.hub.errata.models import ScanningSession
 from osh.hub.errata.scanner import (BaseNotValidException, obtain_base,
                                     prepare_base_scan)
 from osh.hub.scan.models import (SCAN_STATES, AnalyzerVersion, AppSettings,
-                                 Scan, ScanBinding)
+                                 Profile, Scan, ScanBinding)
 from osh.hub.scan.notify import send_task_notification
 from osh.hub.scan.xmlrpc_helper import cancel_scan
 from osh.hub.scan.xmlrpc_helper import fail_scan as h_fail_scan
@@ -128,9 +128,9 @@ def fail_scan(request, scan_id, reason=None):
 
 
 @validate_worker
-def get_scanning_args(request, scanning_session_id):
-    scanning_session = ScanningSession.objects.get(id=scanning_session_id)
-    return scanning_session.profile.command_arguments
+def get_scanning_args(request, profile):
+    # TODO: remove the `or` expression
+    return Profile.objects.get(name=(profile or 'errata')).command_arguments
 
 
 @validate_worker
@@ -150,7 +150,7 @@ def create_sb(request, task_id):
 
 
 @validate_worker
-def ensure_cache(request, mock_config, scanning_session_id):
+def ensure_cache(request, mock_config, profile):
     """
     make sure that cache with version of analyzers is not stale
     """
@@ -158,9 +158,10 @@ def ensure_cache(request, mock_config, scanning_session_id):
         # FIXME: hard-coded at two places for now
         mock_config = 'rhel-9-alpha-x86_64'
     if not AnalyzerVersion.objects.is_cache_uptodate(mock_config):
-        session = ScanningSession.objects.get(id=scanning_session_id)
-        analyzers = session.profile.analyzers
-        csmock_args = session.profile.csmock_args
+        # TODO: remove the `or` expression
+        profile = Profile.objects.get(name=(profile or 'errata'))
+        analyzers = profile.analyzers
+        csmock_args = profile.csmock_args
         su_user = AppSettings.setting_get_su_user()
         return prepare_version_retriever(mock_config, analyzers, su_user, csmock_args)
 

commit 03db5c47ed3d6efdfa9ab881ac1c8f406ba72676

hub: remove all remaining mentions to ScanningSession
Fixes: 460aeea953a1918e55ad2ce4891c573be322297e ("hub: make ErrataDiffBuild task independent of ScanningSession")
Related: https://github.com/openscanhub/openscanhub/pull/170
Related: https://github.com/openscanhub/openscanhub/pull/191

osh/hub/errata/scanner.py
@@ -69,8 +69,6 @@ class AbstractScheduler:
         self.task_args['args'] = {}
         self.task_args['args']['build'] = self.nvr
         self.task_args['args']['profile'] = 'errata'
-        # TODO: remove the 'scanning_session' argument when workers are updated
-        self.task_args['args']['scanning_session'] = None
         self.task_args['args']['su_user'] = AppSettings.setting_get_su_user()
         self.scan_args['nvr'] = self.nvr
         self.scan_args['username'] = self.package_owner

osh/hub/osh_xmlrpc/worker.py
@@ -128,8 +128,7 @@ def fail_scan(request, scan_id, reason=None):
 
 @validate_worker
 def get_scanning_args(request, profile):
-    # TODO: remove the `or` expression
-    return Profile.objects.get(name=(profile or 'errata')).command_arguments
+    return Profile.objects.get(name=profile).command_arguments
 
 
 @validate_worker
@@ -157,8 +156,7 @@ def ensure_cache(request, mock_config, profile):
         # FIXME: hard-coded at two places for now
         mock_config = 'rhel-9-alpha-x86_64'
     if not AnalyzerVersion.objects.is_cache_uptodate(mock_config):
-        # TODO: remove the `or` expression
-        profile = Profile.objects.get(name=(profile or 'errata'))
+        profile = Profile.objects.get(name=profile)
         analyzers = profile.analyzers
         csmock_args = profile.csmock_args
         su_user = AppSettings.setting_get_su_user()