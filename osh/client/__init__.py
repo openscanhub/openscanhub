openscanhub/openscanhub

commit 6d3e3cb3d57b0f4ca442ab919d2b6ee097032041

client: handle the --hub option globally
Related: https://github.com/release-engineering/kobo/pull/192
Resolves: https://gitlab.cee.redhat.com/covscan/covscan/-/issues/171

osh/client/__init__.py
@@ -1,7 +1,23 @@
+import inspect
+
 from kobo.client import ClientCommand
 
 
 class OshCommand(ClientCommand):
+    def connect_to_hub(self, opts):
+        username = opts.pop("username", None)
+        password = opts.pop("password", None)
+        hub = opts.pop("hub", None)
+
+        params = [username, password]
+
+        # For compatibility with older kobo releases
+        sig = inspect.signature(self.set_hub)
+        if len(sig.parameters) == 4:
+            params.append(hub)
+
+        self.set_hub(*params)
+
     def write_task_id_file(self, task_id, filename=None):
         if filename is not None:
             with open(filename, "w") as f:

osh/client/commands/cmd_diff_build.py
@@ -99,8 +99,6 @@ exist." % self.results_store_file)
         local_conf = get_conf(self.conf)
 
         # optparser output is passed via *args (args) and **kwargs (opts)
-        username = kwargs.pop("username", None)
-        password = kwargs.pop("password", None)
         config = kwargs.pop("config", None)
         aggressive = kwargs.pop("aggressive", None)
         cppcheck = kwargs.pop("cppcheck", None)
@@ -154,7 +152,7 @@ is not even one in your user configuration file \
 (/etc/osh/client.conf)")
 
         # login to the hub
-        self.set_hub(username, password)
+        self.connect_to_hub(kwargs)
 
         result = verify_mock(config, self.hub)
         if result is not None:

osh/client/commands/cmd_download_results.py
@@ -36,9 +36,6 @@ class Download_Results(osh.client.OshCommand):
         urllib.request.urlretrieve(url, local_path)
 
     def run(self, *tasks, **kwargs):
-        username = kwargs.pop("username", None)
-        password = kwargs.pop("password", None)
-
         if not tasks:
             self.parser.error("no task ID specified")
 
@@ -52,7 +49,7 @@ class Download_Results(osh.client.OshCommand):
             self.parser.error("provided directory does not exist")
 
         # login to the hub
-        self.set_hub(username, password)
+        self.connect_to_hub(kwargs)
 
         failed = False
 

osh/client/commands/cmd_find_tasks.py
@@ -46,9 +46,6 @@ these options: --regex, --package, --nvr"
         )
 
     def run(self, *args, **kwargs):
-        username = kwargs.pop("username", None)
-        password = kwargs.pop("password", None)
-
         regex = kwargs.pop("regex")
         package_name = kwargs.pop("package")
 
@@ -61,7 +58,7 @@ these options: --regex, --package, --nvr"
         query_string = args[0]
 
         # login to the hub
-        self.set_hub(username, password)
+        self.connect_to_hub(kwargs)
 
         query = {}
         if regex:

osh/client/commands/cmd_list_analyzers.py
@@ -1,5 +1,3 @@
-from kobo.client import HubProxy
-
 import osh.client
 
 
@@ -11,24 +9,10 @@ class List_Analyzers(osh.client.OshCommand):
     def options(self):
         self.parser.usage = f"%prog {self.normalized_name} [options] <args>"
         self.parser.epilog = "list all available static analyzers, some of them in various versions;" + " list contains command line arguments how to enable particular analyzer " + "(e.g. '--analyzer clang' for clang)"
-        self.parser.add_option(
-            "--hub",
-            help="URL of XML-RPC interface on hub; something like \
-https://$hostname/covscan/xmlrpc"
-        )
 
     def run(self, *args, **kwargs):
-        username = kwargs.pop("username", None)
-        password = kwargs.pop("password", None)
-        hub_url = kwargs.pop('hub', None)
-
         # login to the hub
-        if hub_url is None:
-            self.set_hub(username, password)
-        else:
-            self.hub = HubProxy(conf=self.conf,
-                                AUTH_METHOD='krbv',
-                                HUB_URL=hub_url)
+        self.connect_to_hub(kwargs)
 
         format = "%-20s %-20s %-25s"
         columns = ("NAME", "VERSION", "ANALYZER_ID")

osh/client/commands/cmd_list_mock_configs.py
@@ -1,7 +1,5 @@
 import sys
 
-from kobo.client import HubProxy
-
 import osh.client
 
 
@@ -15,25 +13,9 @@ class List_Mock_Configs(osh.client.OshCommand):
         # normalized name contains a lower-case class name with underscores converted to dashes
         self.parser.usage = f"%prog {self.normalized_name} [options] <args>"
 
-        self.parser.add_option(
-            "--hub",
-            help="URL of XML-RPC interface on hub; something like \
-https://$hostname/covscan/xmlrpc"
-        )
-
     def run(self, *args, **kwargs):
-        # optparser output is passed via *args (args) and **kwargs (opts)
-        username = kwargs.pop("username", None)
-        password = kwargs.pop("password", None)
-        hub_url = kwargs.pop('hub', None)
-
         # login to the hub
-        if hub_url is None:
-            self.set_hub(username, password)
-        else:
-            self.hub = HubProxy(conf=self.conf,
-                                AUTH_METHOD='krbv',
-                                HUB_URL=hub_url)
+        self.connect_to_hub(kwargs)
 
         print("NAME", file=sys.stderr)
         for i in self.hub.mock_config.all():

osh/client/commands/cmd_list_profiles.py
@@ -13,11 +13,8 @@ class List_Profiles(osh.client.OshCommand):
                              "One scanning profile could be for C, another for python, shell..."
 
     def run(self, *args, **kwargs):
-        username = kwargs.pop("username", None)
-        password = kwargs.pop("password", None)
-
         # login to the hub
-        self.set_hub(username, password)
+        self.connect_to_hub(kwargs)
 
         format = "%-20s %s"
         columns = ("NAME", "DESCRIPTION")

osh/client/commands/cmd_task_info.py
@@ -17,9 +17,6 @@ class Task_Info(osh.client.OshCommand):
 found"
 
     def run(self, *args, **kwargs):
-        username = kwargs.pop("username", None)
-        password = kwargs.pop("password", None)
-
         if len(args) != 1:
             self.parser.error("please specify exactly one task ID")
 
@@ -28,7 +25,7 @@ found"
             self.parser.error(f"'{task_id}' is not a number")
 
         # login to the hub
-        self.set_hub(username, password)
+        self.connect_to_hub(kwargs)
 
         task_info = self.hub.scan.get_task_info(task_id)
         if not task_info:

commit f5e051c4337fed696481bcff11a4c48243cf2313

client: fix the --hub option
Previously, the code in OSH was not properly detecting the required support
in `kobo`.

Fixes: 6d3e3cb3d57b0f4ca442ab919d2b6ee097032041 ("client: handle the --hub option globally")

osh/client/__init__.py
@@ -16,7 +16,7 @@ class OshCommand(ClientCommand):
 
         # For compatibility with older kobo releases
         sig = inspect.signature(self.set_hub)
-        if len(sig.parameters) == 4:
+        if 'hub' in sig.parameters:
             params.append(hub)
 
         self.set_hub(*params)