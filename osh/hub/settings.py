openscanhub/openscanhub

commit 33696ca0c649b3616103dae2695542e764762a93

hub: restrict user list/detail page access to staff users only
With this update, the user list/detail view will be accessible only to
staff users, this ensures the app meets compliance requirements.

Related: https://github.com/release-engineering/kobo/pull/228
Related: https://github.com/release-engineering/kobo/issues/209
Related: https://issues.redhat.com/browse/OSH-78

osh/hub/settings.py
@@ -202,6 +202,13 @@ def _get_secret(name):
 BZ_API_KEY = _get_secret('bugzilla_secret')
 JIRA_API_KEY = _get_secret('jira_secret')
 
+# Denote whether the access to user list/detail view is restricted
+# Possible values:
+# "" (empty string) = Anonymous access (default)
+# "authenticated" = Authenticated users
+# "staff" = Staff (admin) users only
+USERS_ACL_PERMISSION = "staff"
+
 # read the real SECRET_KEY from SECRET_KEY_FILE if availble
 try:
     with open(SECRET_KEY_FILE) as f:

commit 80221cfde4e56f76d8edc58df1016a900a6ccc27

hub: do not override USERS_ACL_PERMISSION set in settings_local*.py
Fixes: 33696ca0c649b3616103dae2695542e764762a93 (hub: restrict user list/detail page access to staff users only)

osh/hub/settings.py
@@ -179,6 +179,13 @@ LOGIN_EXEMPT_URLS = ['.*xmlrpc/.*']
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 
+# Denote whether the access to user list/detail view is restricted
+# Possible values:
+# "" (empty string) = Anonymous access (default)
+# "authenticated" = Authenticated users
+# "staff" = Staff (admin) users only
+USERS_ACL_PERMISSION = "staff"
+
 VALID_TASK_LOG_EXTENSIONS = ['.log', '.ini', '.err', '.out', '.js', '.txt']
 
 # override default values with custom ones from local settings
@@ -199,12 +206,6 @@ def _get_secret(name):
 BZ_API_KEY = _get_secret('bugzilla_secret')
 JIRA_API_KEY = _get_secret('jira_secret')
 
-# Denote whether the access to user list/detail view is restricted
-# Possible values:
-# "" (empty string) = Anonymous access (default)
-# "authenticated" = Authenticated users
-# "staff" = Staff (admin) users only
-USERS_ACL_PERMISSION = "staff"
 
 # read the real SECRET_KEY from SECRET_KEY_FILE if availble
 try: