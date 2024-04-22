openscanhub/openscanhub

commit 52965d4577cf88be9d1d42facd6a729659c8ef26

hub: make domain name for sending notifications
... to the user configurable.

Resolves: https://github.com/openscanhub/openscanhub/issues/253

Signed-off-by: Siteshwar Vashisht <svashisht@redhat.com>

osh/hub/settings.py
@@ -181,6 +181,10 @@ LOGOUT_REDIRECT_URL = 'index'
 
 VALID_TASK_LOG_EXTENSIONS = ['.log', '.ini', '.err', '.out', '.js', '.txt']
 
+# This is kept here for backward compatibility.
+# https://github.com/openscanhub/openscanhub/pull/256#pullrequestreview-2001187953
+DEFAULT_EMAIL_DOMAIN = "@redhat.com"
+
 # override default values with custom ones from local settings
 try:
     from .settings_local import *  # noqa

osh/hub/scan/notify.py
@@ -56,8 +56,7 @@ def get_recipient(user):
     if user.email:
         return user.email
 
-    # XXX: hardcoded
-    return user.username + "@redhat.com"
+    return f"{user.username}@{settings.DEFAULT_EMAIL_DOMAIN}"
 
 
 def generate_stats(task, diff_task=False, with_defects_in_patches=False):

osh/hub/settings_local.py
@@ -130,4 +130,8 @@ NOTIFICATION_EMAIL_ADDRESS = "no-reply@example.org"
 # E-mail address where notifications about failed/cancelled tasks are sent
 DEVEL_EMAIL_ADDRESS = "no-reply@example.org"
 
+# Default e-mail domain name for sending notifications to the user.
+# It is used when `email` field in the user model is not set.
+DEFAULT_EMAIL_DOMAIN = "example.org"
+
 ALLOWED_HOSTS = ['*']

commit a38ce52ecef6566a2b11067633b70bc315a1c8c4

hub: fix a copy-paste error in the default e-mail domain
`DEFAULT_EMAIL_DOMAIN` should not contain the leading `@` because
the concatenated string then contains `user@@redhat.com`, which
results in difficult-to-debug failures to send e-mail notifications:
```
Apr 22 00:02:52 cov01.lab.eng.brq2.redhat.com postfix/smtpd[2713480]: connect from localhost[::1]
Apr 22 00:02:53 cov01.lab.eng.brq2.redhat.com postfix/smtpd[2713480]: lost connection after CONNECT from localhost[::1]
Apr 22 00:02:53 cov01.lab.eng.brq2.redhat.com postfix/smtpd[2713480]: disconnect from localhost[::1] commands=0/0
Apr 22 00:05:08 cov01.lab.eng.brq2.redhat.com postfix/smtpd[2714548]: connect from localhost[::1]
Apr 22 00:05:08 cov01.lab.eng.brq2.redhat.com postfix/smtpd[2714548]: lost connection after CONNECT from localhost[::1]
Apr 22 00:05:08 cov01.lab.eng.brq2.redhat.com postfix/smtpd[2714548]: disconnect from localhost[::1] commands=0/0
Apr 22 00:12:55 cov01.lab.eng.brq2.redhat.com postfix/smtpd[2718052]: connect from localhost[::1]
Apr 22 00:13:09 cov01.lab.eng.brq2.redhat.com postfix/smtpd[2718052]: lost connection after CONNECT from localhost[::1]
Apr 22 00:13:09 cov01.lab.eng.brq2.redhat.com postfix/smtpd[2718052]: disconnect from localhost[::1] commands=0/0
```

Fixes: commit 52965d4577cf88be9d1d42facd6a729659c8ef26
Closes: https://github.com/openscanhub/openscanhub/pull/260

osh/hub/settings.py
@@ -183,7 +183,7 @@ VALID_TASK_LOG_EXTENSIONS = ['.log', '.ini', '.err', '.out', '.js', '.txt']
 
 # This is kept here for backward compatibility.
 # https://github.com/openscanhub/openscanhub/pull/256#pullrequestreview-2001187953
-DEFAULT_EMAIL_DOMAIN = "@redhat.com"
+DEFAULT_EMAIL_DOMAIN = "redhat.com"
 
 # override default values with custom ones from local settings
 try: