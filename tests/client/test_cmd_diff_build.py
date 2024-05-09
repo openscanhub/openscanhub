ce2e5cf86c11e6ddcdc8b560cd22674e516b991d
Yuguang Wang <yuwang@redhat.com>
2024-05-09 11:16:43 +0800

    client: add test cases for diff-build
    Related: https://issues.redhat.com/browse/OSH-160

---
+++ tests/client/test_cmd_diff_build.py
@@ -0,0 +1,52 @@
+import optparse
+import unittest
+from io import StringIO
+from unittest.mock import MagicMock, patch
+
+from osh.client.commands.cmd_diff_build import Diff_Build
+
+
+class TestDiffBuild(unittest.TestCase):
+    def setUp(self):
+        parser = optparse.OptionParser()
+        parser.add_option = MagicMock()
+
+        self.command = Diff_Build(parser=parser)
+        self.command.hub = MagicMock()
+        self.command.container = MagicMock()
+        self.command.normalized_name = "diff-build"
+
+    @patch('osh.client.commands.cmd_diff_build.Base_Build.prepare_task_options')
+    def test_prepare_task_options(self, mock_super_prepare_task_options):
+        with patch('sys.stderr', new=StringIO()) as fake_err:
+            # Error if no SRPM/NVR is specified
+            args = []
+            with self.assertRaises(SystemExit) as cm:
+                self.command.prepare_task_options(args, {})
+            self.assertEqual(cm.exception.code, 2)
+            output = fake_err.getvalue()
+            self.assertIn("please specify either SRPM or NVR", output)
+
+        with patch('sys.stderr', new=StringIO()) as fake_err:
+            # Error if length of args is larger than 1
+            args = ["srpm1", "srpm2"]
+            with self.assertRaises(SystemExit) as cm:
+                self.command.prepare_task_options(args, {})
+            self.assertEqual(cm.exception.code, 2)
+            output = fake_err.getvalue()
+            self.assertIn("please specify exactly one SRPM", output)
+
+        args = ["srpm1"]
+        kwargs = {}
+        self.command.prepare_task_options(args, kwargs)
+        self.assertIn("srpm", kwargs)
+        mock_super_prepare_task_options.assert_called_once()
+
+    def test_submit_task(self):
+        self.command.hub.scan.diff_build.return_value = 0
+        options = {
+            "mock_config": "rhel-7-x86_64",
+            "comment": "my comment"
+        }
+        self.command.submit_task(options)
+        self.command.hub.scan.diff_build.assert_called_once()
