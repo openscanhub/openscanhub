2b99559d0dfb6fd434297f33fb49ec6cbda97c19
Yuguang Wang <yuwang@redhat.com>
2024-05-10 16:57:25 +0800

    client: add git-url option in mock-build
    Related: https://issues.redhat.com/browse/OSH-592

--- osh/client/commands/cmd_mock_build.py
+++ osh/client/commands/cmd_mock_build.py
@@ -2,7 +2,8 @@
 # SPDX-FileCopyrightText: Copyright contributors to the OpenScanHub project.
 
 from osh.client.commands.cmd_diff_build import Diff_Build
-from osh.client.commands.common import add_tarball_option
+from osh.client.commands.common import (add_dist_git_url_option,
+                                        add_tarball_option)
 
 
 class Mock_Build(Diff_Build):
@@ -15,6 +16,7 @@ class Mock_Build(Diff_Build):
 
         # this option cannot be used for diff-build tasks
         add_tarball_option(self.parser)
+        add_dist_git_url_option(self.parser)
 
     def submit_task(self, options):
         return self.hub.scan.mock_build(options['mock_config'],
