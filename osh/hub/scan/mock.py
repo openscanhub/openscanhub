8bb2b03674c8535613f952e3d82f717ffe9c4cb4
Lukáš Zaoral <lzaoral@redhat.com>
2024-06-19 10:29:23 +0200

    hub: fix generation of mock configs for noarch builds
    ... because `Arch` of a task corresponds to the arch of the worker
    and not the arch of the actual build.

--- osh/hub/scan/mock.py
+++ osh/hub/scan/mock.py
@@ -112,18 +112,32 @@ def _get_module_build_tag(koji_proxy, task_id):
     return build_tag
 
 
-def _get_build_arches(koji_proxy, task_id):
+def _get_build_arches(koji_proxy, task_id, tag):
     """
-    returns all arches available for given build
+    returns all arches available for given build and tag
     """
     arches = []
     for child_id in koji_proxy.getTaskDescendents(task_id, request=True):
         child = koji_proxy.getTaskInfo(child_id, request=True)
-        if child['method'] == 'buildArch' and child['arch'] != 'noarch':
-            arches.append(child['arch'])
 
-    # noarch tasks can be built anywhere
-    return arches or koji_proxy.getAllArches()
+        # skip non-build subtasks
+        method = child['method']
+        if method != 'buildArch':
+            continue
+
+        # parse arch
+        arch = koji.parse_task_params(method, child['request'])['arch']
+
+        # handle noarch builds
+        if arch == 'noarch':
+            # get all arches supported by given tag
+            # (which are stored in a string for some reason...)
+            return koji_proxy.getTag(tag)['arches'].split()
+
+        # otherwise append
+        arches.append(arch)
+
+    return arches
 
 
 def _get_tag_specific_config(target):
@@ -220,7 +234,7 @@ def generate_mock_configs(nvr, koji_profile):
 
     # generate a config for every built arch
     logger.debug(f'Generating mock configs for build tag "{tag}"')
-    for arch in _get_build_arches(koji_proxy, task['id']):
+    for arch in _get_build_arches(koji_proxy, task['id'], tag):
         _create_mock_config(tag, arch, koji_profile, tmpdir)
 
     return tmpdir
